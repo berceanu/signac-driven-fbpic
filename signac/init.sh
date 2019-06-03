bde966f9074cce305c13d145690fe57a01d4226d
berceanu <andrei.berceanu@eli-np.ro>
2019-06-03 14:36:34 +0300

    supress file not found warnings

--- signac/init.sh
+++ signac/init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 rm -rf .bundles/
-rm fbpic-minimal-project.log
-rm signac.rc
-rm signac_statepoints.json
+rm -f fbpic-minimal-project.log
+rm -f signac.rc
+rm -f signac_statepoints.json
 rm -rf workspace/
 
 eval "$(conda shell.bash hook)"
