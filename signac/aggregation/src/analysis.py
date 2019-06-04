3e62e1665cd2195ff2f85b82ab43a94fdf5cdca4
berceanu <andreicberceanu@gmail.com>
2019-06-04 15:52:27 +0300

    testing postprocessing on original script

--- signac/aggregation/src/analysis.py
+++ signac/aggregation/src/analysis.py
@@ -4,19 +4,22 @@
 # import psutil
 # import itertools
 import os
-# from multiprocessing import Pool
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
 
 import h5py
 import matplotlib as mpl
 
 # import matplotlib.colors as colors
 import numpy as np
+from opmd_viewer import OpenPMDTimeSeries
 from scipy.constants import physical_constants
 
-from opmd_viewer import OpenPMDTimeSeries
 import fbpic_script  # fb-pic parameter file, located in the same folder
 import postproc.plotz as plotz
 import postproc.postpic as pp
+
+# from multiprocessing import Pool
+
 
 mpl.rc("lines", linewidth=2)
 mpl.rc("axes", labelsize=20, titlesize=20, linewidth=1.5)
@@ -44,7 +47,7 @@ q_e = physical_constants["elementary charge"][0]  # C, positive
 c = physical_constants["speed of light in vacuum"][0]  # m/s
 m_e = physical_constants["electron mass"][0]  # Kg
 mc2 = m_e * c ** 2 / (q_e * 1e6)  # electron rest energy in MeV: 0.511
-tstep_to_pos = (c * 1e-9) * dt  # conversion factor in mu
+tstep_to_pos = (c * 1e-9) * dt  # conversion factor in mu (Δz)
 
 n_e = fbpic_script.n_e  # initial electron density in electrons x m^{-3}
 n_c = 1.75e27  # critical electron density in electrons x m^{-3}
@@ -57,16 +60,30 @@ ts_circ = OpenPMDTimeSeries(h5_path, check_all_files=False)
 
 
 def field_snapshot(
-        tseries,
-        iteration,
-        field_name,
-        norm_factor,
-        coord=None,
-        m="all",
-        theta=0.0,
-        chop=None,
-        **kwargs
-):
+    tseries: OpenPMDTimeSeries,
+    iteration: int,
+    field_name: str,
+    norm_factor: float,
+    coord: Optional[str] = None,
+    m="all",
+    theta=0.0,
+    chop: Optional[List[float]] = None,
+    **kwargs: Union[int, str, float]
+) -> None:
+    """
+    Plot the ``field_name`` field from ``tseries`` at step ``iteration``.
+
+    :param tseries: whole simulation time series
+    :param iteration: time step in the simulation
+    :param field_name: which field to extract, eg. 'rho', 'E', 'B' or 'J'
+    :param norm_factor: normalization factor for the extracted field
+    :param coord: which component of the field to extract, eg. 'r', 't' or 'z'
+    :param m: 'all' for extracting the sum of all the modes
+    :param theta: the angle of the plane of observation, with respect to the x axis
+    :param chop: adjusting extent of simulation box plot
+    :param kwargs: extra plotting arguments, eg. labels, data limits etc.
+    :return: saves field plot image to disk
+    """
     if chop is None:
         chop = [0.0, 0.0, 0.0, 0.0]
     field, info = tseries.get_field(
@@ -96,23 +113,54 @@ def field_snapshot(
 
 
 def particle_histogram(
-        tseries,
-        iteration,
-        energy_min=1.0,
-        energy_max=300.0,
-        nbins=100,
-):
+    tseries: OpenPMDTimeSeries,
+    iteration: int,
+    energy_min=1.0,
+    energy_max=300.0,
+    nbins=100,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Compute the weighted particle energy histogram from ``tseries`` at step ``iteration``.
+
+    :param tseries: whole simulation time series
+    :param iteration: time step in the simulation
+    :param energy_min: lower energy threshold
+    :param energy_max: upper energy threshold
+    :param nbins: number of bins
+    :return: histogram values and bin edges
+    """
     delta_energy = (energy_max - energy_min) / nbins
-    energy_bins = np.linspace(energy_min, energy_max, nbins + 1)
+    energy_bins = np.linspace(start=energy_min, stop=energy_max, num=nbins + 1)
+
     ux, uy, uz, w = tseries.get_particle(["ux", "uy", "uz", "w"], iteration=iteration)
     energy = mc2 * np.sqrt(1 + ux ** 2 + uy ** 2 + uz ** 2)  # Energy in MeV
+
     q_bins, edges = np.histogram(
-        energy, energy_bins, weights=q_e * 1e12 / delta_energy * w
+        energy, bins=energy_bins, weights=q_e * 1e12 / delta_energy * w
     )  # weights in pC/MeV
-    return q_bins.tolist(), edges.tolist()
+
+    return q_bins, edges
 
 
-def apply_func(iteration):
+def apply_func(
+    iteration
+) -> Tuple[int, float, float, float, float, float, np.ndarray, np.ndarray]:
+    """
+    Computes z₀, a₀, w₀, cτ, energy histogram and plots particle density for the field ``"rho"``.
+
+    :param iteration: time step in the simulation
+    :return: time step, corresponding time, z₀, a₀, w₀, cτ, energy histogram
+    """
+    z_0, a_0, w_0, c_tau = pp.get_a0(ts_circ, iteration=iteration)  # SI
+
+    q_bins, edges = particle_histogram(
+        tseries=ts_circ,
+        iteration=iteration,
+        energy_min=1.0,
+        energy_max=350.0,
+        nbins=349,
+    )
+
     field_snapshot(
         tseries=ts_circ,
         iteration=iteration,
@@ -123,29 +171,43 @@ def apply_func(iteration):
         vmin=0,
         vmax=3,
     )
+
     time_fs = iteration * dt
-    z_0, a_0, w_0, c_tau = pp.get_a0(ts_circ, iteration=iteration)  # SI
-    q_bins_list, edg_list = particle_histogram(
-        tseries=ts_circ, iteration=iteration, energy_min=1.0, energy_max=350.0, nbins=349
-    )
-    return iteration, time_fs, z_0 * 1e6, a_0, w_0 * 1e6, c_tau * 1e6, q_bins_list, edg_list  # microns
+    return (
+        iteration,
+        time_fs,
+        z_0 * 1e6,
+        a_0,
+        w_0 * 1e6,
+        c_tau * 1e6,
+        q_bins,
+        edges,
+    )  # microns
+
+
+def extract(i: int, lst_of_tuples: List[Tuple]) -> np.ndarray:
+    """
+    Extract every ``i``th item from a list of tuples.
+
+    :param i: item position inside a tuple
+    :param lst_of_tuples: list of equal-length tuples [(..,#i,..),(..,#i,..),..]
+    :return: array of extracted values [#i, #i, ..]
+    """
+    return np.array([tple[i] for tple in lst_of_tuples])
 
 
 if __name__ == "__main__":
     print(ts_circ.iterations[-1], tstep_to_pos)
 
+    # loop over all diagnostic time steps and apply ``apply_func()``
     diags = list()
     for it in ts_circ.iterations.tolist():
         diags.append(apply_func(it))
 
-
     # pool = Pool()
     # diags = pool.map(apply_func, ts_circ.iterations.tolist())
 
-    def extract(i, lst_of_tuples):
-        return np.array([tple[i] for tple in lst_of_tuples])
-
-
+    # extract results
     z0 = extract(2, diags)
     a0 = extract(3, diags)
     w0 = extract(4, diags)
@@ -153,6 +215,7 @@ if __name__ == "__main__":
     Q = extract(6, diags)
     # edg = extract(7, diags)
 
+    # plot a₀ vs z₀
     plot_1d = plotz.Plot1D(
         a0,
         z0,
@@ -164,7 +227,8 @@ if __name__ == "__main__":
         color="red",
     )
     plot_1d.canvas.print_figure("a0.png")  #
-    #
+
+    # plot w₀ vs z₀
     plot_1d = plotz.Plot1D(
         w0,
         z0,
@@ -173,17 +237,19 @@ if __name__ == "__main__":
         figsize=(10, 6),
     )
     plot_1d.canvas.print_figure("w0.png")  #
-    #
+
+    # plot cτ vs z₀
     plot_1d = plotz.Plot1D(
         ctau, z0, xlabel=r"$%s \;(\mu m)$" % "z", ylabel=r"$c \tau \;(\mu m)$"
     )
     plot_1d.canvas.print_figure("ctau.png")  #
 
+    # plot 2D energy histogram
     z_min = ts_circ.iterations[0] * tstep_to_pos + 70  # mu
     z_max = (
-            ts_circ.iterations[-1] * tstep_to_pos + 70
+        ts_circ.iterations[-1] * tstep_to_pos + 70
     )  # offset by 70 because left margin is at -70
-    z_35100 = ts_circ.iterations[35100 // 100] * tstep_to_pos + 70 # error here!
+    z_35100 = ts_circ.iterations[35100 // 100] * tstep_to_pos + 70  # error here!
     #
     h_axis = np.linspace(z_min, z_max, ts_circ.iterations.size - 1)
     v_axis = np.linspace(1.0, 350.0, 349)
@@ -224,6 +290,8 @@ if __name__ == "__main__":
     # chop = [40, -20, 15, -15],
     # zlabel=r'$n_e/n_c$', vmin=0., vmax=0.1,
     # hslice_val=0,)# vslice_val=840.5)
+
+    # plot electric field Ex
     field_snapshot(
         tseries=ts_circ,
         iteration=35100,
