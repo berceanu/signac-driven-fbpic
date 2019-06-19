4bf4b40dcedd3321d3d2c41210375b0d1f4e3c12
berceanu <andreicberceanu@gmail.com>
2019-06-19 15:38:01 +0300

    cleaned up PostProc package

--- PostProc/postproc/__init__.py
+++
@@ -1,2 +0,0 @@
-# todo check if these modules are used
-# todo update install instructions for lwfa and plotz packages
--- PostProc/setup.py
+++ PostProc/setup.py
@@ -2,19 +2,10 @@ from setuptools import setup
 
 setup(
    name='PostProc',
-   version='0.1.0',
+   version='0.0.1',
    author='Andrei Berceanu',
    author_email='andrei.berceanu@eli-np.ro',
    packages=['postproc'],
-#    scripts=['bin/script1','bin/script2'],
-#    url='http://pypi.python.org/pypi/PackageName/',
-   license='LICENSE.md',
-   description='Package containing modules for post-processing fbpic simulation output',
-   long_description=open('README.md').read(),
-   install_requires=[
-       "signac >= 1.1.0",
-       "signac-flow >= 0.7.1",
-       "signac-dashboard >= 0.2.3",
-       "fbpic >= 0.12.0"
-   ],
+   description='Package containing unused modules',
+   install_requires=[],
 )
