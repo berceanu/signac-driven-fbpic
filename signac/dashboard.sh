464dc741da483090eed9ed0d1cb3f3a4ec889d92
berceanu <andrei.berceanu@eli-np.ro>
2019-10-09 17:39:05 +0300

    Remove conda environment from dashboard

--- signac/dashboard.sh
+++ signac/dashboard.sh
@@ -3,7 +3,4 @@
 # Launch the web interface for result visualization.
 # Usage: ./dashboard.sh
 
-eval "$(conda shell.bash hook)"
-conda activate signac-driven-fbpic
-
-python3 src/dashboard.py run --port 7777
+python src/dashboard.py run --port 7777
