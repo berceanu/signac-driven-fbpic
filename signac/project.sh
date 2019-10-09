033ecf9a6527c58d0fe96714d2e20b8f154e8932
berceanu <andrei.berceanu@eli-np.ro>
2019-10-09 13:10:41 +0300

    Remove usage of conda environment

--- signac/project.sh
+++ signac/project.sh
@@ -3,14 +3,11 @@
 # Run the project operations, in parallel, using at most N GPUs and all CPU cores.
 # Usage: ./project.sh N
 
-eval "$(conda shell.bash hook)"
-conda activate signac-driven-fbpic
-
 # parallel on N GPUs
-python3 src/project.py submit --bundle=$1 --parallel --test | /bin/bash
+python src/project.py submit --bundle=$1 --parallel --test | /bin/bash
 
 # complete remaining CPU operations, using all cores
-python3 src/project.py run --parallel
+python src/project.py run --parallel
 
 # todo use https://github.com/glotzerlab/signac-flow/pull/114
 
--- signac/status.sh
+++ signac/status.sh
@@ -3,7 +3,4 @@
 # Check job completion status while jobs are running.
 # Usage: ./status.sh
 
-eval "$(conda shell.bash hook)"
-conda activate signac-driven-fbpic
-
-python3 src/project.py status --pretty --full --stack
+python src/project.py status --pretty --full --stack
