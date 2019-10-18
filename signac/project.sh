1d8df71999a80ae5344f7aaa712e54893ad90471
Andrei Berceanu <andreicberceanu@gmail.com>
2019-10-18 21:03:19 +0200

    Split diagnostics to separate script

--- signac/project.sh
+++ signac/project.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# Run the project operations, in parallel, using at most N GPUs and all CPU cores.
+# Run the project operations, in parallel, using at most N GPUs.
 # Usage: ./project.sh N
 
 # parallel on N GPUs
 python src/project.py submit --bundle=$1 --parallel --test | /bin/bash
-
-# complete remaining CPU operations, using all cores
-python src/project.py run --parallel
 
