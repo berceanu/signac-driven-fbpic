edd44b4ff9f936ed11feebba8d3462e97856ccf4
Andrei Berceanu <andreicberceanu@gmail.com>
2019-10-21 16:36:33 +0200

    Notebooks working

--- jupyter/holoviews_examples.py
+++ jupyter/holoviews_examples.py
@@ -1,11 +1,11 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:percent
+#     formats: ipynb,py:light
 #     text_representation:
 #       extension: .py
-#       format_name: percent
-#       format_version: '1.2'
+#       format_name: light
+#       format_version: '1.4'
 #       jupytext_version: 1.2.4
 #   kernelspec:
 #     display_name: Python 3
@@ -13,33 +13,24 @@
 #     name: python3
 # ---
 
-# %%
 import numpy as np
 import holoviews as hv
 
-# %%
 hv.extension('bokeh')
 
-# %%
-from holoviews import streams
+from holoviews import streams, opts, dim
 
-# %%
-from holoviews import opts
-
-# %% [markdown]
 # ## 2D plots with interactive slicing
 
-# %%
 # ls = np.linspace(0, 10, 200)
 xx, yy = np.meshgrid(ls, ls)
 bounds=(0,0,10,10)   # Coordinate system: (left, bottom, right, top)
 
-# %%
 energy = hv.Dimension('energy', label='E', unit='MeV')
 distance = hv.Dimension('distance', label='d', unit='m')
 charge = hv.Dimension('charge', label='Q', unit='pC')
 
-# %%
+# +
 image = hv.Image(np.sin(xx)*np.cos(yy), bounds=bounds, kdims=[energy, distance], vdims=charge)
 pointer = streams.PointerXY(x=5,y=5, source=image)
 
@@ -61,21 +52,18 @@ layout.opts(
     opts.HLine(color='lightsalmon'),
     opts.Points(color='red', marker='o', size=10)
 ).cols(3)
+# -
 
-# %% [markdown]
 # ## Interactive integration
 
-# %%
 xs = np.linspace(-3, 3, 400)
 
 
-# %%
 def function(xs, time):
     "Some time varying function"
     return np.exp(np.sin(xs+np.pi/time))
 
 
-# %%
 def integral(limit_a, limit_b, y, time):
     limit_a = -3 if limit_a is None else np.clip(limit_a,-3,3)
     limit_b = 3 if limit_b is None else np.clip(limit_b,-3,3)
@@ -85,29 +73,23 @@ def integral(limit_a, limit_b, y, time):
     return (area * curve * hv.VLine(limit_a) * hv.VLine(limit_b) * hv.Text(limit_b - 0.8, 2.0, '%.2f' % summed))
 
 
-# %%
 integral_streams = [
     streams.Stream.define('Time', time=1.0)(),
     streams.PointerX().rename(x='limit_b'),
     streams.Tap().rename(x='limit_a')
 ]
 
-# %%
 integral_dmap = hv.DynamicMap(integral, streams=integral_streams)
 
-# %%
 integral_dmap.opts(
     opts.Area(color='#fff8dc', line_width=2),
     opts.Curve(color='black'),
     opts.VLine(color='red'))
 
-# %%
 image.dimensions()
 
-# %%
 dim('charge').min().apply(image)
 
-# %%
 dim('charge').max().apply(image)
 
-# %%
+
--- jupyter/postprocessing.py
+++ jupyter/postprocessing.py
@@ -2,11 +2,11 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:percent
+#     formats: ipynb,py:light
 #     text_representation:
 #       extension: .py
-#       format_name: percent
-#       format_version: '1.2'
+#       format_name: light
+#       format_version: '1.4'
 #       jupytext_version: 1.2.4
 #   kernelspec:
 #     display_name: Python 3
@@ -14,23 +14,15 @@
 #     name: python3
 # ---
 
-# %%
-import holoviews as hv
-from holoviews import dim, opts, streams, param
 
-hv.extension('bokeh')
-
-# %%
+# +
 import os
 import numpy as np
 import signac
 
 from opmd_viewer import OpenPMDTimeSeries
 
-# %%
-np.set_printoptions(precision=2, linewidth=80)
-
-# %%
+# +
 # ugly hack to import project.py from 'signac/src'
 import sys
 
@@ -39,46 +31,42 @@ sys.path.insert(0, os.path.join(
 from project import particle_energy_histogram
 
 del sys.path[0], sys
+# -
 
-# %%
+import holoviews as hv
+from holoviews import dim, opts, streams, param
+
+hv.extension('bokeh')
+np.set_printoptions(precision=2, linewidth=80)
+
 pr = signac.get_project(root="../signac", search=False)
 
-# %%
 # We see there are a few values of a0 in the project. We now want the job id of the job with a certain a0 value.
 job_id_set = pr.find_job_ids({'a0': 3})
 job_id = next(iter(job_id_set))
 print(job_id)  # this is also the name of the job's workspace folder
 
-# %%
 # What are the full job parameters?
 print(pr.get_statepoint(job_id))
 
-# %%
 # get the job handler
 job = pr.open_job(id=job_id)
 
-# %%
 # We can now access individual parameters, like so
 print(job.sp.N_step)
 
-# %%
 # get path to job's hdf5 files
 h5_path = os.path.join(job.ws, "diags", "hdf5")
 print(h5_path)
 
-# %%
 # open the full time series and see iteration numbers
 time_series = OpenPMDTimeSeries(h5_path, check_all_files=True)
 print(time_series.iterations)
 iteration = time_series.iterations[-1]
 print(iteration)
 
-# %%
 help(particle_energy_histogram)
 
-# %%
-
-# %%
 # compute 1D histogram
 energy_hist, bin_edges, nbins = particle_energy_histogram(
     tseries=time_series,
@@ -86,23 +74,20 @@ energy_hist, bin_edges, nbins = particle_energy_histogram(
     cutoff=np.inf,  # no cutoff
 )
 
-# %%
 delta_E = bin_edges[1] - bin_edges[0]  # MeV
 
-# %%
 energy = hv.Dimension('energy', label='E', unit='MeV')
 count = hv.Dimension('frequency', label='dQ/dE', unit='pC/MeV')
 
-# %%
 histogram = hv.Histogram((bin_edges, energy_hist), kdims=energy, vdims=count)
 curve = hv.Curve(histogram)
 
-# %%
+# +
 e_min = dim('energy').min().apply(curve)
 e_max = dim('energy').max().apply(curve)
 
 
-def integral(limit_a, limit_b, y):
+def integral(limit_a, limit_b, y, iteration):
     limit_a = e_min if limit_a is None else np.clip(limit_a, e_min, e_max)
     limit_b = e_max if limit_b is None else np.clip(limit_b, e_min, e_max)
     area = hv.Area((curve.dimension_values('energy'), curve.dimension_values('frequency')))[limit_a:limit_b]
@@ -110,20 +95,17 @@ def integral(limit_a, limit_b, y):
     return curve * area * hv.VLine(limit_a) * hv.VLine(limit_b) * hv.Text(limit_b - 2., 5, 'Q = %.0f pC' % charge)
 
 
-# %%
+# -
+
 integral_streams = [
     streams.Stream.define('Iteration', iteration=param.Integer(default=4600, doc='Time step in the simulation'))(),
     streams.PointerX().rename(x='limit_b'),
     streams.Tap().rename(x='limit_a')
 ]
 
-# %%
 integral_dmap = hv.DynamicMap(integral, streams=integral_streams)
 
-# %%
 integral_dmap.opts(
     opts.Area(color='#fff8dc', line_width=2),
     opts.Curve(color='black', height=300, responsive=True, show_grid=True, xlim=(None, 25), ylim=(None, 10)),
     opts.VLine(color='red'))
-
-# %%
