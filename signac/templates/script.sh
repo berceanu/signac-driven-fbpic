802462d52318f932b75d203d34fb2bce0abfb97e
Andrei Berceanu <andreicberceanu@gmail.com>
2020-06-13 00:39:23 +0300

    Remove no-slurm script

--- signac/templates/script.sh
+++
@@ -1,6 +0,0 @@
-{% set cmd_suffix = cmd_suffix|default('') ~ (' &' if parallel else '') %}
-{% for operation in operations %}
-export CUDA_VISIBLE_DEVICES={{ loop.index0 }}
-{{ operation.cmd }}{{ cmd_suffix }}
-{% endfor %}
-wait
