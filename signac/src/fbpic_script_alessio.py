99a8f5840b126e8668b08742891105827513a11b
Andrei Berceanu <andreicberceanu@gmail.com>
2020-11-11 23:20:41 +0200

    Implement changes from fbpic bunch example

--- signac/src/fbpic_script_alessio.py
+++ signac/src/fbpic_script_alessio.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.constants import c, e, m_e
+from scipy.constants import c, e, m_e, m_p
 
 # Import the relevant structures in FBPIC
 from fbpic.main import Simulation
@@ -8,8 +8,8 @@ from fbpic.openpmd_diag import FieldDiagnostic, ParticleDiagnostic
 
 # The simulation box
 Nz = 8400  # Number of gridpoints along z
-zmax = 3000.0e-6  # Right end of the simulation box (meters)
-zmin = -39000.0e-6  # Left end of the simulation box (meters)
+zmax = 3000.0e-6  # Length of the box along z (meters)
+zmin = -39000.0e-6
 Nr = 250  # Number of gridpoints along r
 rmax = 500.0e-6  # Length of the box along r (meters)
 Nm = 3  # Number of modes used
@@ -17,14 +17,14 @@ Nm = 3  # Number of modes used
 # The simulation timestep
 dt = (zmax - zmin) / Nz / c  # Timestep (seconds)
 
-# The particles
+# The particles of the plasma
 p_zmin = 30.0e-6  # Position of the beginning of the plasma (meters)
-p_zmax = 68000.0e-6  # Position of the end of the plasma (meters)
+p_zmax = 68000.0e-6
 p_rmax = 400.0e-6  # Maximal radial position of the plasma (meters)
-n_e = 5.0e13 * 1.0e6  # Density (electrons.meters^-3)
+n_e = 5.0e13 * 1.0e6  # The density (electrons.meters^-3)
 p_nz = 2  # Number of particles per cell along z
 p_nr = 2  # Number of particles per cell along r
-p_nt = 4  # Number of particles per cell along theta
+p_nt = 6  # Number of particles per cell along theta
 
 # The electron beam
 L0 = 100.0e-6  # Position at which the beam should be "unfreezed"
@@ -37,7 +37,7 @@ v_window = c  # Speed of the window
 diag_period = 1000  # Period of the diagnostics in number of timesteps
 
 # The density profile
-def dens_func_ext(z, r):
+def dens_func(z, r):
     # Allocate relative density
     n = np.ones_like(z)
     # Read density data and nomalize
@@ -57,7 +57,7 @@ def dens_func_ext(z, r):
 
 
 # The interaction length of the simulation (meters)
-L_interact = 68000.0e-6  # increase to simulate longer distance!
+L_interact = p_zmax - p_zmin  # the plasma length
 # Interaction time (seconds) (to calculate number of PIC iterations)
 T_interact = (L_interact + (zmax - zmin)) / v_window
 # (i.e. the time it takes for the moving window to slide across the plasma)
@@ -73,17 +73,31 @@ if __name__ == "__main__":
         Nm,
         dt,
         zmin=zmin,
-        boundaries={"z": "open", "r": "reflective"},
+        boundaries={"z": "open", "r": "open"},
         n_order=-1,
         use_cuda=True,
     )
+    # 'r': 'open' can also be used, but is more computationally expensive
 
-    # Create the plasma electrons
-    _ = sim.add_new_species(
+    
+    # Add the plasma electron and plasma ions
+    plasma_elec = sim.add_new_species(
         q=-e,
         m=m_e,
         n=n_e,
-        dens_func=dens_func_ext,
+        dens_func=dens_func,
+        p_zmin=p_zmin,
+        p_zmax=p_zmax,
+        p_rmax=p_rmax,
+        p_nz=p_nz,
+        p_nr=p_nr,
+        p_nt=p_nt,
+    )
+    plasma_ions = sim.add_new_species(
+        q=e,
+        m=m_p,
+        n=n_e,
+        dens_func=dens_func,
         p_zmin=p_zmin,
         p_zmax=p_zmax,
         p_rmax=p_rmax,
@@ -93,7 +107,7 @@ if __name__ == "__main__":
     )
 
     # particles beam from txt file
-    beam = add_particle_bunch_file(
+    bunch = add_particle_bunch_file(
         sim,
         q=-e,
         m=m_e,
@@ -108,8 +122,9 @@ if __name__ == "__main__":
 
     # Add diagnostics
     sim.diags = [
-        FieldDiagnostic(diag_period, sim.fld, comm=sim.comm),
-        ParticleDiagnostic(diag_period, {"beam": beam}, comm=sim.comm),
+        FieldDiagnostic(dt_period=diag_period, fieldtypes=["rho", "E"], fldobject=sim.fld, comm=sim.comm),
+        ParticleDiagnostic(dt_period=diag_period, species={"electrons": plasma_elec, "bunch": bunch}, comm=sim.comm),
+        # TODO select based on particle velocity?
     ]
 
     # Number of iterations to perform
