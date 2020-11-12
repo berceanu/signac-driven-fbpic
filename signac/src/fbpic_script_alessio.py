8b95c16ab5cd075dbc4c2ab3b2c83180d7cfe0c6
Andrei Berceanu <andreicberceanu@gmail.com>
2020-11-12 14:15:12 +0200

    Separate bunch visualization via datashader

--- signac/src/fbpic_script_alessio.py
+++ signac/src/fbpic_script_alessio.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from scipy.constants import c, e, m_e, m_p
-from scipy.constants import physical_constants
 from scipy import interpolate
 
 # Import the relevant structures in FBPIC
@@ -9,10 +8,6 @@ from fbpic.main import Simulation
 from fbpic.lpa_utils.bunch import add_particle_bunch_file
 from fbpic.openpmd_diag import FieldDiagnostic, ParticleDiagnostic
 
-c_light = physical_constants["speed of light in vacuum"][0]
-m_e = physical_constants["electron mass"][0]
-q_e = physical_constants["elementary charge"][0]
-mc2 = m_e * c_light ** 2 / (q_e * 1e6)  # 0.511 MeV
 
 
 # The simulation box
@@ -45,31 +40,6 @@ v_window = c  # Speed of the window
 # The diagnostics
 diag_period = 1000  # Period of the diagnostics in number of timesteps
 
-def read_bunch(txt_file):
-    df = pd.read_csv(
-        txt_file,
-        delim_whitespace=True,
-        names=["x_m", "y_m", "z_m", "ux", "uy", "uz"],
-    )
-    # convert to microns
-    df["x_mu"] = df.x_m * 1e+6
-    df["y_mu"] = df.y_m * 1e+6
-    df["z_mu"] = df.z_m * 1e+6
-
-    # remove first 3 columns
-    df = df.drop(["x_m", "y_m", "z_m"], axis=1)
-
-    # compute gamma factor
-    df["gamma"] = np.sqrt(1 + df.ux ** 2 + df.uy ** 2 + df.uz ** 2)
-
-    # compute energy
-    df["energy_MeV"] = mc2 * df.gamma
-
-    df["percent_c"] = np.sqrt(1 - 1 / df.gamma ** 2) * 100.0
-
-    return df
-
-df = read_bunch("../exp_4deg.txt")
 
 def read_density(txt_file, every_nth=20, offset=True):
     df = pd.read_csv(
@@ -167,7 +137,6 @@ if __name__ == "__main__":
         sim,
         q=-e,
         m=m_e,
-        # x [m]  y [m]  z [m]  ux [unitless]  uy [unitless]  uz [unitless]
         filename="../exp_4deg.txt",
         n_physical_particles=Qtot / e,
         z_off=0.0,
---
+++ signac/src/show_bunch_file.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from scipy.constants import physical_constants
+import datashader as ds
+import datashader.transfer_functions as tf
+from datashader.utils import export_image
+from functools import partial
+from datashader.colors import colormap_select
+from colorcet import fire
+
+c_light = physical_constants["speed of light in vacuum"][0]
+m_e = physical_constants["electron mass"][0]
+q_e = physical_constants["elementary charge"][0]
+mc2 = m_e * c_light ** 2 / (q_e * 1e6)  # 0.511 MeV
+
+def read_bunch(txt_file):
+    df = pd.read_csv(
+        txt_file,
+        delim_whitespace=True,
+        names=["x_m", "y_m", "z_m", "ux", "uy", "uz"],
+    )
+    # convert to microns
+    df["x_mu"] = df.x_m * 1e+6
+    df["y_mu"] = df.y_m * 1e+6
+    df["z_mu"] = df.z_m * 1e+6
+
+    # remove first 3 columns
+    df = df.drop(["x_m", "y_m", "z_m"], axis=1)
+
+    # compute gamma factor
+    df["gamma"] = np.sqrt(1 + df.ux ** 2 + df.uy ** 2 + df.uz ** 2)
+
+    # compute energy
+    df["energy_MeV"] = mc2 * df.gamma
+
+    df["percent_c"] = np.sqrt(1 - 1 / df.gamma ** 2) * 100.0
+
+    return df
+
+df = read_bunch("../exp_4deg.txt")
+
+# plot via datashader
+ds.transfer_functions.Image.border=0
+background = "black"
+cm = partial(colormap_select, reverse=(background!="black"))
+export = partial(export_image, background = background, export_path="img")
+
+def shade_bunch(coord1, coord2):
+    cvs = ds.Canvas(plot_width=700, plot_height=700)
+    agg = cvs.points(df, coord1, coord2)
+    img = tf.shade(agg, cmap = cm(fire,0.2), how='linear')
+    export(img, f"bunch_{coord1}_{coord2}")
+
+shade_bunch('x_mu', 'y_mu')
+shade_bunch('y_mu', 'z_mu')
+shade_bunch('x_mu', 'z_mu')
+
+print(df)
