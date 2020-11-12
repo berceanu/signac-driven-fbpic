4958360d8c71384ac6acf8cf7dbce61cbfc73ba1
Andrei Berceanu <andreicberceanu@gmail.com>
2020-11-12 15:49:55 +0200

    Separate density reading function

--- signac/src/fbpic_script_alessio.py
+++ signac/src/fbpic_script_alessio.py
@@ -7,8 +7,7 @@ from scipy import interpolate
 from fbpic.main import Simulation
 from fbpic.lpa_utils.bunch import add_particle_bunch_file
 from fbpic.openpmd_diag import FieldDiagnostic, ParticleDiagnostic
-
-
+from .reader import read_density
 
 # The simulation box
 Nz = 8400  # Number of gridpoints along z
@@ -39,33 +38,6 @@ v_window = c  # Speed of the window
 
 # The diagnostics
 diag_period = 1000  # Period of the diagnostics in number of timesteps
-
-
-def read_density(txt_file, every_nth=20, offset=True):
-    df = pd.read_csv(
-        txt_file,
-        delim_whitespace=True,
-        names=["position_mu", "density_cm_3", "TODO_ask_alessio"],
-    )
-
-    # convert to meters
-    df["position_m"] = df.position_mu * 1e-6
-
-    # substract offset
-    if not offset:
-        df.position_m = df.position_m - df.position_m.iloc[0]
-
-    # normalize density
-    df["norm_density"] = df.density_cm_3 / df.density_cm_3.max()
-    # check density values between 0 and 1
-    if not df.norm_density.between(0, 1).any():
-        raise ValueError("The density contains values outside the range [0,1].")
-
-    # return every nth item
-    df = df.iloc[::every_nth, :]
-
-    # return data as numpy arrays
-    return df.position_m.to_numpy(), df.norm_density.to_numpy()
 
 
 position_m, norm_density = read_density("../density_1_inlet_spacers.txt")
