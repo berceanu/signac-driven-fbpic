58366647d5b5b5ec3e673521a7f9e1b63dcdcb72
Andrei Berceanu <andreicberceanu@gmail.com>
2020-11-23 23:53:15 +0200

    Add missing LWFA diagnostics

--- signac/src/project.py
+++ signac/src/project.py
@@ -13,19 +13,21 @@ Note: All the lines marked with the CHANGEME comment contain customizable parame
 import logging
 import math
 import os
-import sys
 import subprocess
 import glob
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Iterable, Callable, Tuple
 
 import numpy as np
+import pandas as pd
 import sliceplots
 from flow import FlowProject, directives
 from flow.environment import DefaultSlurmEnvironment
 from matplotlib import pyplot
 from openpmd_viewer import OpenPMDTimeSeries
 from scipy.constants import physical_constants
+from scipy.signal import hilbert
 from signac.contrib.job import Job
+
 
 logger = logging.getLogger(__name__)
 log_file_name = "fbpic-project.log"
@@ -74,6 +76,16 @@ class OdinEnvironment(DefaultSlurmEnvironment):
 #####################
 # UTILITY FUNCTIONS #
 #####################
+
+
+def are_files(file_names: Iterable[str]) -> Callable[[Job], bool]:
+    """Check if given file names are in the ``job`` dir.
+    Useful for pre- and post- operation conditions.
+
+    :param file_names: iterable containing file names
+    :return: anonymous function that does the check
+    """
+    return lambda job: all(job.isfile(fn) for fn in file_names)
 
 
 def sh(*cmd, **kwargs) -> str:
@@ -305,7 +317,7 @@ def run_fbpic(job: Job) -> None:
         zmin=job.sp.zmin,
         boundaries={"z": "open", "r": "open"},
         n_order=-1,
-        use_cuda=False,
+        use_cuda=True,
         verbose_level=2,
     )
     # 'r': 'open' can also be used, but is more computationally expensive
@@ -453,6 +465,45 @@ def electric_field_amplitude_norm(lambda0=0.8e-6):
     return e0
 
 
+def particle_energy_histogram(
+        tseries: OpenPMDTimeSeries, it: int,
+        energy_min=1, energy_max=800, delta_energy=1, cutoff=35,  # CHANGEME
+):
+    """
+    Compute the weighted particle energy histogram from ``tseries`` at step ``iteration``.
+
+    :param tseries: whole simulation time series
+    :param it: time step in the simulation
+    :param energy_min: lower energy threshold (MeV)
+    :param energy_max: upper energy threshold (MeV)
+    :param delta_energy: size of each energy bin (MeV)
+    :param cutoff: upper threshold for the histogram, in pC / MeV
+    :return: histogram values and bin edges
+    """
+    nbins = (energy_max - energy_min) // delta_energy
+    energy_bins = np.linspace(start=energy_min, stop=energy_max, num=nbins + 1)
+
+    #FIXME get_particle now returns particle positions in meters instead of microns
+    ux, uy, uz, w = tseries.get_particle(["ux", "uy", "uz", "w"], iteration=it)
+    energy = mc2 * np.sqrt(1 + ux ** 2 + uy ** 2 + uz ** 2)
+
+    # Explanation of weights:
+    #     1. convert electron charge from C to pC (factor 1e12)
+    #     2. multiply by weight w to get real number of electrons
+    #     3. divide by energy bin size delta_energy to get charge / MeV
+    hist, _ = np.histogram(
+        energy, bins=energy_bins, weights=q_e * 1e12 / delta_energy * w
+    )
+
+    # cut off histogram
+    np.clip(hist, a_min=None, a_max=cutoff, out=hist)
+
+    return hist, energy_bins, nbins
+
+
+
+
+
 def field_snapshot(
     tseries: OpenPMDTimeSeries,
     it: int,
@@ -509,14 +560,91 @@ def field_snapshot(
     filename = os.path.join(path, f"{field_name}{it:06d}.png")
     plot.canvas.print_figure(filename)
 
+def get_a0(
+        tseries: OpenPMDTimeSeries,
+        t: Optional[float] = None,
+        it: Optional[int] = None,
+        coord="x",
+        m="all",
+        slice_across=None,
+        theta=0.0,
+        lambda0=0.8e-6,
+) -> Tuple[float, float, float, float]:
+    """
+    Compute z₀, a₀, w₀, cτ.
+
+    :param tseries: whole simulation time series
+    :param t: time (in seconds) at which to obtain the data
+    :param it: time step at which to obtain the data
+    :param coord: which component of the field to extract
+    :param m: 'all' for extracting the sum of all the modes
+    :param slice_across: the direction along which to slice the data eg., 'x', 'y' or 'z'
+    :param theta: the angle of the plane of observation, with respect to the 'x' axis
+    :param lambda0: laser wavelength (meters)
+    :return: z₀, a₀, w₀, cτ
+    """
+    # get E_x field in V/m
+    electric_field_x, info_electric_field_x = tseries.get_field(
+        field="E",
+        coord=coord,
+        t=t,
+        iteration=it,
+        m=m,
+        theta=theta,
+        slice_across=slice_across,
+    )
+
+    # normalized vector potential
+    e0 = electric_field_amplitude_norm(lambda0=lambda0)
+    a0 = electric_field_x / e0
+
+    # get pulse envelope
+    envelope = np.abs(hilbert(a0, axis=1))
+    envelope_z = envelope[envelope.shape[0] // 2, :]
+
+    a0_max = np.amax(envelope_z)
+
+    # index of peak
+    z_idx = np.argmax(envelope_z)
+    # pulse peak position
+    z0 = info_electric_field_x.z[z_idx]
+
+    # FWHM perpendicular size of beam, proportional to w0
+    fwhm_a0_w0 = (
+            np.sum(np.greater_equal(envelope[:, z_idx], a0_max / 2))
+            * info_electric_field_x.dr
+    )
+
+    # FWHM longitudinal size of the beam, proportional to ctau
+    fwhm_a0_ctau = (
+            np.sum(np.greater_equal(envelope_z, a0_max / 2)) * info_electric_field_x.dz
+    )
+
+    return z0, a0_max, fwhm_a0_w0, fwhm_a0_ctau
 
 @ex
 @Project.operation
 @Project.pre.after(run_fbpic)
+@Project.post(are_files(("diags.txt", "all_hist.txt", "hist_edges.txt")))
 @Project.post(are_rho_pngs)
 def post_process_results(job: Job) -> None:
     """
     Loop through a whole simulation and, for *each ``fbpic`` iteration*:
+
+    a. compute
+
+        1. the iteration time ``it_time``
+        2. position of the laser pulse peak ``z_0``
+        3. normalized vector potential ``a_0``
+        4. beam waist ``w_0``
+        5. spatial pulse length ``c_tau``
+
+    and write results to "diags.txt".
+
+    b. compute the weighted particle energy histogram and save it to "all_hist.txt",
+    and the histogram bins to "hist_edges.txt"
+
+    c. save a snapshot of the plasma density field ``rho`` to {job_dir}/diags/rhos/rho{it:06d}.png
 
     :param job: the job instance is a handle to the data of a unique statepoint
     """
@@ -524,9 +652,41 @@ def post_process_results(job: Job) -> None:
     rho_path: Union[bytes, str] = os.path.join(job.ws, "rhos")
 
     time_series: OpenPMDTimeSeries = OpenPMDTimeSeries(h5_path, check_all_files=False)
+    number_of_iterations: int = time_series.iterations.size
+
+    # Do a mock histogram in order to get the number of bins
+    _, _, nrbins = particle_energy_histogram(
+        tseries=time_series,
+        it=0,
+    )
+    all_hist = np.empty(shape=(number_of_iterations, nrbins), dtype=np.float64)
+    hist_edges = np.empty(shape=(nrbins + 1,), dtype=np.float64)
+
+    diags_file = open(job.fn("diags.txt"), "w")
+    diags_file.write("iteration,time[fs],z₀[μm],a₀,w₀[μm],cτ[μm]\n")
 
     # loop through all the iterations in the job's time series
-    for it in time_series.iterations:
+    for idx, it in enumerate(time_series.iterations):
+        it_time = it * job.sp.dt
+
+        z_0, a_0, w_0, c_tau = get_a0(
+            time_series, it=it, lambda0=job.sp.lambda0
+        )
+
+        diags_file.write(
+            f"{it:06d},{it_time * 1e15:.3e},{z_0 * 1e6:.3e},{a_0:.3e},{w_0 * 1e6:.3e},{c_tau * 1e6:.3e}\n"
+        )
+
+        # generate 1D energy histogram
+        energy_hist, bin_edges, _ = particle_energy_histogram(
+            tseries=time_series,
+            it=it,
+        )
+        # build up arrays for 2D energy histogram
+        all_hist[idx, :] = energy_hist
+        if idx == 0:  # only save the first one
+            hist_edges[:] = bin_edges
+
         # save "rho{it:06d}.png"
         field_snapshot(
             tseries=time_series,
@@ -535,8 +695,8 @@ def post_process_results(job: Job) -> None:
             normalization_factor=1.0 / (-q_e * job.sp.n_e),
             path=rho_path,
             zlabel=r"$n/n_e$",
-            vmin=0.0,
-            vmax=2.0,  # CHANGEME
+            vmin=0,
+            vmax=2,  # CHANGEME
             hslice_val=0,
         )
 
@@ -545,6 +705,97 @@ def post_process_results(job: Job) -> None:
     # multiply by electron charge -q_e to get (N e) / V
     # so we get Q / N e, which is C/C, i.e. dimensionless
     # Note: one can also normalize by the critical density n_c
+
+    diags_file.close()
+
+    np.savetxt(
+        job.fn("all_hist.txt"),
+        all_hist,
+        header="One iteration per row, containing the energy histogram.",
+    )
+    np.savetxt(job.fn("hist_edges.txt"), hist_edges, header="Energy histogram bins.")
+
+@ex
+@Project.operation
+@Project.pre(are_files(("diags.txt", "all_hist.txt", "hist_edges.txt")))
+@Project.post.isfile("hist2d.png")
+def plot_2d_hist(job: Job) -> None:
+    """
+    Plot the 2D histogram, composed of the 1D slices for each iteration.
+
+    :param job: the job instance is a handle to the data of a unique statepoint
+    """
+    df_diags = pd.read_csv(job.fn("diags.txt"), header=0, index_col=0, comment="#")
+    all_hist = np.loadtxt(
+        job.fn("all_hist.txt"), dtype=np.float64, comments="#", ndmin=2
+    )
+    hist_edges = np.loadtxt(
+        job.fn("hist_edges.txt"), dtype=np.float64, comments="#", ndmin=1
+    )
+
+    z_0 = df_diags.loc[:, "z₀[μm]"]
+
+    # plot 2D energy-charge histogram
+    hist2d = sliceplots.Plot2D(
+        arr2d=all_hist.T,  # 2D data
+        h_axis=z_0.values,  # x-axis
+        v_axis=hist_edges[1:],  # y-axis
+        xlabel=r"$%s \;(\mu m)$" % "z",
+        ylabel=r"E (MeV)",
+        zlabel=r"dQ/dE (pC/MeV)",
+        vslice_val=z_0.iloc[-1],  # can be changed to z_0.loc[iteration]
+        extent=(z_0.iloc[0], z_0.iloc[-1], hist_edges[1], hist_edges[-1]),
+    )
+    hist2d.canvas.print_figure(job.fn("hist2d.png"))
+
+
+@ex
+@Project.operation
+@Project.pre.isfile("diags.txt")
+@Project.post(are_files(("a0.png", "w0.png", "ctau.png")))
+def plot_1d_diags(job: Job) -> None:
+    """
+    Plot the 1D diagnostics, ``a_0``, ``w_0`` and ``c_tau`` vs ``z_0``.
+
+    :param job: the job instance is a handle to the data of a unique statepoint
+    """
+    df_diags = pd.read_csv(job.fn("diags.txt"), header=0, index_col=0, comment="#")
+
+    z_0 = df_diags.loc[:, "z₀[μm]"].values
+    a_0 = df_diags.loc[:, "a₀"].values
+    w_0 = df_diags.loc[:, "w₀[μm]"].values
+    c_tau = df_diags.loc[:, "cτ[μm]"].values
+
+    fig, ax = pyplot.subplots(figsize=(10, 6))
+    sliceplots.plot1d(
+        ax=ax,
+        v_axis=a_0,  # y-axis
+        h_axis=z_0,  # x-axis
+        xlabel=r"$%s \;(\mu m)$" % "z",
+        ylabel=r"$%s$" % "a_0",
+        ylim=[0, 10],  # CHANGEME
+    )
+    fig.savefig(job.fn("a0.png"))
+
+    fig, ax = pyplot.subplots(figsize=(10, 6))
+    sliceplots.plot1d(
+        ax=ax,
+        v_axis=w_0,  # y-axis
+        h_axis=z_0,  # x-axis
+        xlabel=r"$%s \;(\mu m)$" % "z",
+        ylabel=r"$%s \;(\mu m)$" % "w_0",
+    )
+    fig.savefig(job.fn("w0.png"))
+
+    fig, ax = pyplot.subplots(figsize=(10, 6))
+    sliceplots.plot1d(
+        ax=ax,
+        v_axis=c_tau,  # y-axis
+        h_axis=z_0,  # x-axis
+        xlabel=r"$%s \;(\mu m)$" % "z",
+        ylabel=r"$c \tau \;(\mu m)$",
+    )
+    fig.savefig(job.fn("ctau.png"))
 
 
 @ex
