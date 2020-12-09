17c2ceb26456306416777a4abeb8f5e99ba4106e
Andrei Berceanu <andreicberceanu@gmail.com>
2020-12-09 23:29:26 +0200

    minor edit

--- signac/src/peak_detection.py
+++ signac/src/peak_detection.py
@@ -1,16 +1,15 @@
-from cycler import cycler
 import numpy as np
 import sliceplots
 from matplotlib import pyplot
+
 from collections import defaultdict
+from cycler import cycler
 
 line_colors = ["C1", "C2", "C3", "C4", "C5", "C6"]
 line_styles = ["-", "--", ":", "-.", (0, (1, 10)), (0, (5, 10))]
 
 cyl = cycler(color=line_colors) + cycler(linestyle=line_styles)
-
 loop_cy_iter = cyl()
-
 STYLE = defaultdict(lambda: next(loop_cy_iter))
 
 
