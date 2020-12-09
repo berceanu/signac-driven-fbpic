ca560f222242149e7f341669bbd2dbdc1a5a2d59
Andrei Berceanu <andreicberceanu@gmail.com>
2020-12-09 02:36:06 +0200

    add signac dashboard

---
+++ signac/src/dashboard.py
@@ -0,0 +1,31 @@
+from signac_dashboard import Dashboard
+from signac_dashboard.modules import (
+    StatepointList,
+    DocumentList,
+    ImageViewer,
+    VideoViewer,
+)
+import unyt as u
+
+
+class MyDashboard(Dashboard):
+    def job_sorter(self, job):
+        return job.sp.zfoc
+
+    def job_title(self, job):
+        zfoc = (job.sp.zfoc * u.meter).to_value("micrometer")
+        return f"zfoc = {zfoc:.1f} um"
+
+    # def job_subtitle(self, job):
+    #     pass
+
+
+if __name__ == "__main__":
+    modules = [
+        StatepointList(name="Parameters", enabled=False),
+        DocumentList(enabled=False),
+        ImageViewer(enabled=False),
+        ImageViewer(name="2D Histogram", img_globs=["hist2d.png"]),
+        VideoViewer(enabled=False),
+    ]
+    MyDashboard(modules=modules).main()
