39a8d0cdcd8b4f9873bc8080fd2c26f80fb20a7b
Andrei Berceanu <andreicberceanu@gmail.com>
2020-12-09 03:12:34 +0200

    minor edit

--- signac/src/dashboard.py
+++ signac/src/dashboard.py
@@ -1,7 +1,6 @@
 from signac_dashboard import Dashboard
 from signac_dashboard.modules import (
     StatepointList,
-    DocumentList,
     ImageViewer,
     VideoViewer,
 )
@@ -23,7 +22,6 @@ class MyDashboard(Dashboard):
 if __name__ == "__main__":
     modules = [
         StatepointList(name="Parameters", enabled=False),
-        DocumentList(enabled=False),
         ImageViewer(enabled=False),
         ImageViewer(name="2D Histogram", img_globs=["hist2d.png"]),
         VideoViewer(enabled=False),
