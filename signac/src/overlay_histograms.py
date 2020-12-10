9cfd47353570fc10f0b4dee76797f5f9130a94e3
Andrei Berceanu <andreicberceanu@gmail.com>
2020-12-10 15:37:10 +0200

    finalize plots

--- signac/src/overlay_histograms.py
+++ signac/src/overlay_histograms.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import numpy as np
 from matplotlib import pyplot
+from matplotlib.ticker import MultipleLocator
 from cycler import cycler
 from scipy.constants import golden
 import unyt as u
@@ -16,12 +17,12 @@ STYLE = defaultdict(lambda: next(loop_cy_iter))
 def main():
     proj = signac.get_project(search=False)
 
+    peak_position_charge = list()
+
     fig, ax = pyplot.subplots(figsize=(golden * 8, 8))
 
     ax.set_xlabel("E (MeV)")
     ax.set_ylabel("dQ/dE (pC/MeV)")
-
-    peak_position_charge = list()
 
     for zf, jobs in proj.groupby(key="zfoc"):
         job = next(jobs)  # assuming single job per group
@@ -54,22 +55,29 @@ def main():
             linewidth=0.5,
         )
 
-    ax.legend()
+    ax.legend(frameon=False)
+    ax.axvline(x=160, color="0.75", linestyle="dotted")
+
     fig.savefig("histograms.png", dpi=192, transparent=False)
 
     zfoc, peak_position, peak_charge = zip(*peak_position_charge)
 
-    fig, ax = pyplot.subplots(figsize=(golden * 8, 8))
-    ax.plot(zfoc, peak_position)
-    ax.set_xlabel(r"$z_f$ ($\mathrm{\mu m}$)")
-    ax.set_ylabel("E (MeV)")
-    fig.savefig("positions.png")
+    fig, ax1 = pyplot.subplots(figsize=(golden * 8, 8))
+    ax2 = ax1.twinx()
 
-    fig, ax = pyplot.subplots(figsize=(golden * 8, 8))
-    ax.plot(zfoc, peak_charge)
-    ax.set_xlabel(r"$z_f$ ($\mathrm{\mu m}$)")
-    ax.set_ylabel("Q (pC)")
-    fig.savefig("charges.png")
+    ax1.axhline(y=160, color="C1", linestyle="dotted")
+    ax1.plot(zfoc, peak_position, "o--", color="C1")
+    ax2.plot(zfoc, peak_charge, "o--", color="C0")
+
+    ax1.set_xlabel(r"$z_f$ ($\mathrm{\mu m}$)")
+    ax1.set_ylabel("E (MeV)", color="C1")
+    ax2.set_ylabel("Q (pC)", color="C0")
+
+    ax1.xaxis.set_major_locator(MultipleLocator(50))
+
+    ax1.grid(which="major", axis="x", linewidth=0.75, linestyle="dashed", color="0.75")
+
+    fig.savefig("positions_charges.png")
 
 
 if __name__ == "__main__":
