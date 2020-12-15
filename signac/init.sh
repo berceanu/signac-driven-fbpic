5b9910c1c98f689cd567c5e4c36cc5a5dbe276a3
Andrei Berceanu <andreicberceanu@gmail.com>
2020-12-15 14:56:06 +0200

    remove local workspace folder

--- signac/init.sh
+++ signac/init.sh
@@ -9,5 +9,6 @@ rm -f signac.rc
 rm -f .signac_sp_cache.json.gz
 rm -f signac_project_document.json
 # rm -rf /scratch/berceanu/runs/signac-driven-fbpic/workspace/ FIXME
+rm -rf workspace/
 
 python src/init.py
