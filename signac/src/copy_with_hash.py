56818f619623c6bb828a7cab306c94bd9c6bb019
Andrei Berceanu <andreicberceanu@gmail.com>
2021-01-12 14:36:39 +0200

    Restore script for copying from workspace

---
+++ signac/src/copy_with_hash.py
@@ -0,0 +1,26 @@
+import pathlib
+import signac
+
+project = signac.get_project()
+
+
+def copy_with_hash(path_to_fname, dst_dir, signac_job):
+    fname = path_to_fname.stem
+    ext = path_to_fname.suffix
+
+    dst_fname = dst_dir / f"{fname}_{signac_job.id:.6}{ext}"
+    dst_fname.write_bytes(path_to_fname.read_bytes())
+
+
+if __name__ == "__main__":
+    out_path = pathlib.Path.cwd() / "runs"
+    out_path.mkdir(parents=True, exist_ok=True)
+
+    statepoints = pathlib.Path.cwd() / "signac_statepoints.json"
+    p = out_path / "signac_statepoints.json"
+    p.write_bytes(statepoints.read_bytes())
+
+    for job in project:
+        for f_name in ("bunch/final_bunch.txt", "rho.mp4", "centroid.mp4"):
+            src = pathlib.Path(job.fn(f_name))
+            copy_with_hash(src, out_path, job)
