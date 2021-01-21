1293171f30caf25d69ef8086da9607ddcbae88f4
Andrei Berceanu <andreicberceanu@gmail.com>
2021-01-22 00:00:16 +0200

    Add aggregation over proj

---
+++ signac/src/displacement.py
@@ -0,0 +1,82 @@
+import pathlib
+import numpy as np
+from matplotlib import pyplot
+from scipy.constants import golden
+import unyt as u
+from openpmd_viewer.addons import LpaDiagnostics
+from simulation_diagnostics import (
+    plot_spline_derivatives,
+    centroid_plot,
+    compute_bending_energy,
+)
+import signac
+
+
+def plot_vs_density(x, y, ylabel="", fn="out.png", up_to=None):
+    if up_to is not None:
+        x = x[:up_to].copy()
+        y = y[:up_to].copy()
+
+    fig, ax = pyplot.subplots(figsize=(golden * 4, 4))
+
+    ax.plot(x, y, "*--", linewidth=1)
+
+    ax.set_xscale("log")
+    ax.set_xlabel(r"$n_e$ (cm${}^{-3}$)")
+    ax.set_ylabel(ylabel)
+
+    ax.grid()
+
+    fig.savefig(fn, bbox_inches="tight")
+    pyplot.close(fig)
+
+
+def main():
+    proj = signac.get_project(search=False)
+    N = len(proj)
+
+    job_densities = np.empty(N) / u.cm ** 3
+    job_centroid_positions = np.empty(N) * u.micrometer
+    job_bending_energies = np.empty(N) / u.micrometer
+
+    counter = 0
+
+    for _, jobs in proj.groupby(key="n_e"):
+        job = next(jobs)  # assuming single job per group
+
+        ne = job.sp.n_e / u.meter ** 3
+        job_densities[counter] = ne.to(1 / u.cm ** 3)
+
+        # get final iteration
+        h5_path = pathlib.Path(job.ws) / "diags" / "hdf5"
+        time_series = LpaDiagnostics(h5_path, check_all_files=True)
+        it = time_series.iterations[-1]
+
+        x_avg = centroid_plot(iteration=it, tseries=time_series)[2] * u.meter
+        job_centroid_positions[counter] = x_avg.to(u.micrometer)
+
+        # plot_spline_derivatives(iteration=it, tseries=time_series)
+
+        W = compute_bending_energy(iteration=it, tseries=time_series) / u.meter
+        job_bending_energies[counter] = W.to(u.micrometer ** (-1))
+
+        counter += 1
+
+    plot_vs_density(
+        job_densities,
+        job_centroid_positions,
+        up_to=19,
+        ylabel=r"$\langle x \rangle$ ($\mu$m)",
+        fn="average_centroids.png",
+    )
+    plot_vs_density(
+        job_densities,
+        job_bending_energies,
+        up_to=19,
+        ylabel=r"$W$ ($\mu$m${}^{-1}$)",
+        fn="bending_energies.png",
+    )
+
+
+if __name__ == "__main__":
+    main()
