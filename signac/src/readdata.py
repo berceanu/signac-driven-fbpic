dcae34a35a3993483cf953be19510e10cf673d8e
Andrei Berceanu <andreicberceanu@gmail.com>
2021-01-24 16:43:01 +0200

    Remove inline comments

--- signac/src/readdata.py
+++ signac/src/readdata.py
@@ -1,6 +1,6 @@
 import pathlib
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot
 
 
 def readbeam(datadir):
@@ -16,7 +16,9 @@ def readbeam(datadir):
     x_min = min(x)
     x_max = max(x)
 
-    h = plt.hist2d(z, x, bins=(nbz, nbx), range=[[z_min, z_max], [x_min, x_max]])
+    fig, ax = pyplot.subplots()
+
+    h = ax.hist2d(z, x, bins=(nbz, nbx), range=[[z_min, z_max], [x_min, x_max]])
     counts = h[0]
     z_coords = np.linspace(z_min, z_max, nbz)
     x_coords = np.linspace(x_min, x_max, nbx)
@@ -34,22 +36,19 @@ def readbeam(datadir):
     for i in range(0, nbz):
         counts[i, :][counts[i, :] < 0.15 * refval] = 0
         if max(counts[i, :]) > 0.2 * refval and i > 20 and i < 180:
-            # print(np.argmax(counts[i,:]))
-            # centroid.append(x_coords[np.argmax(counts[i,:])])
             centroid.append(np.average(x_coords, weights=counts[i, :]))
             centroid_z.append(z_coords[i])
             we.append(sum(counts[i, :]))
 
-    plt.plot(centroid_z, centroid)
-    plt.xlabel("z (m)", fontsize=20)
-    plt.ylabel("x (m)", fontsize=20)
-    plt.show()
+    ax.plot(centroid_z, centroid)
+    ax.set_xlabel("z (m)")
+    ax.set_ylabel("x (m)")
 
-    # mean_head = np.mean(centroid[len(centroid)-5:len(centroid)])
+    fig.savefig("alessio.png", bbox_inches="tight")
+    pyplot.close(fig)
+
     m_cent = abs(np.mean(centroid))
     m_cent_w = abs(np.average(centroid, weights=we))
-    # print(m_cent)#
-    # print(m_cent_w)
 
     return m_cent, m_cent_w
 
