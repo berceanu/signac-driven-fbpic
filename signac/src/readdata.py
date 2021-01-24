6b35a984bb5f8bb34563849545954058c60fd9c1
Andrei Berceanu <andreicberceanu@gmail.com>
2021-01-24 16:32:21 +0200

    Remove junk

--- signac/src/readdata.py
+++ signac/src/readdata.py
@@ -1,20 +1,13 @@
-import os
 import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import UnivariateSpline
-from scipy.integrate import simps
 
 
 def readbeam(datadir):
     data = np.loadtxt(datadir, skiprows=1)
 
-    y = data[:, 1]
     x = data[:, 0]
     z = data[:, 2]
-    ux = data[:, 3]
-    uy = data[:, 4]
-    uz = data[:, 5]
 
     nbz = 190
     nbx = 190
@@ -33,7 +26,6 @@ def readbeam(datadir):
     centroid = []
     we = []
     centroid_z = []
-    slope = []
 
     for i in range(0, nbz):
         if refval < max(counts[i, :]):
@@ -48,21 +40,10 @@ def readbeam(datadir):
             centroid_z.append(z_coords[i])
             we.append(sum(counts[i, :]))
 
-    y_spl = UnivariateSpline(centroid_z, centroid, s=0, k=4)
-    x_range = np.linspace(centroid_z[0], centroid_z[-1], 1000)
-
-    plt.plot(x_range, y_spl(x_range))
-
     plt.plot(centroid_z, centroid)
     plt.xlabel("z (m)", fontsize=20)
     plt.ylabel("x (m)", fontsize=20)
     plt.show()
-
-    y_spl_2d = y_spl.derivative(n=2)
-    y2 = y_spl_2d(x_range) ** 2
-    In = simps(y2, x_range)
-    # plt.plot(x_range,y2)
-    # plt.show()
 
     # mean_head = np.mean(centroid[len(centroid)-5:len(centroid)])
     m_cent = abs(np.mean(centroid))
@@ -70,14 +51,14 @@ def readbeam(datadir):
     # print(m_cent)#
     # print(m_cent_w)
 
-    return m_cent, m_cent_w, In
+    return m_cent, m_cent_w
 
 
 def main():
     p = pathlib.Path.cwd() / "final_bunch_66dc81.txt"
 
-    m_cent, m_cent_w, In = readbeam(p)
-    print(m_cent_w)
+    m_cent, m_cent_w = readbeam(p)
+    print(m_cent, m_cent_w)
 
 
 if __name__ == "__main__":
