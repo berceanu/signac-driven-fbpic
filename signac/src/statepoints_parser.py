28786dfc82143f276d734958ff750b44982865b3
Andrei Berceanu <andreicberceanu@gmail.com>
2021-01-26 18:38:16 +0200

    Create helper functions

--- signac/src/statepoints_parser.py
+++ signac/src/statepoints_parser.py
@@ -12,35 +12,53 @@ def to_cm_minus_three(n_e):
     return (n_e / u.meter ** 3).to(1 / u.centimeter ** 3)
 
 
-def main():
-    """Main entry point."""
-    statepoints_path = pathlib.Path.home() / "tmp" / "runs" / "signac_statepoints.json"
-    statepoints = json.loads(statepoints_path.read_bytes())
+def load_statepoints(statepoints_path):
+    json_path = statepoints_path / "signac_statepoints.json"
+    statepoints = json.loads(json_path.read_bytes())
+    return statepoints
 
+
+def shorten_id(d):
+    return {f"{k:.6}": v for k, v in d.items()}
+
+
+def change_density_units(d):
+    return {k: to_cm_minus_three(v) for k, v in d.items()}
+
+
+def map_id_to_density(statepoints):
     id_to_density = {}
     for signac_job_id, job_state_point in statepoints.items():
         id_to_density[signac_job_id] = job_state_point["n_e"]
+    return id_to_density
 
-    short_id_to_density_cm3 = {
-        f"{id:.6}": to_cm_minus_three(density) for id, density in id_to_density.items()
-    }
+
+def sort_by_value(d):
+    return dict(sorted(d.items(), key=lambda item: item[1]))
+
+
+def main():
+    """Main entry point."""
+    statepoints = load_statepoints(pathlib.Path.home() / "tmp" / "runs")
+    id_to_density = map_id_to_density(statepoints)
+
+    d = shorten_id(id_to_density)
+    short_id_to_density_cm3 = change_density_units(d)
 
     bunch_fn_to_density = {
         f"final_bunch_{id}.txt": density
         for id, density in short_id_to_density_cm3.items()
     }
+    sorted_bunch_fn_to_density = sort_by_value(bunch_fn_to_density)
 
-    sorted_bunch_fn_to_density = dict(
-        sorted(bunch_fn_to_density.items(), key=lambda item: item[1])
-    )
-
+    # #
     for fn, n_e in sorted_bunch_fn_to_density.items():
         print(f"{n_e:.1e} -> {fn}")
 
+    print()  #
     runs_dir = pathlib.Path.home() / "tmp" / "runs"
     txt_files = runs_dir.glob("final_bunch_*.txt")
 
-    print()
     for fn in txt_files:
         print(f"{fn.name}: {sorted_bunch_fn_to_density[fn.name]:.1e}")
 
