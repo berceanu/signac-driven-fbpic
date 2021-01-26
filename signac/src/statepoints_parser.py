921195a273dd836addf9007392dc57ff2c7cbb81
Andrei Berceanu <andreicberceanu@gmail.com>
2021-01-26 18:05:58 +0200

    Loop though final_bunch files

--- signac/src/statepoints_parser.py
+++ signac/src/statepoints_parser.py
@@ -15,7 +15,6 @@ def to_cm_minus_three(n_e):
 def main():
     """Main entry point."""
     statepoints_path = pathlib.Path.home() / "tmp" / "runs" / "signac_statepoints.json"
-    print(statepoints_path)
     statepoints = json.loads(statepoints_path.read_bytes())
 
     # The end-goal is to get a dictionary of the form
@@ -31,22 +30,20 @@ def main():
     for signac_job_id, job_state_point in statepoints.items():
         id_to_density[signac_job_id] = job_state_point["n_e"]
 
-    print(id_to_density)
-    print()
-
     short_id_to_density_cm3 = {
         f"{id:.6}": to_cm_minus_three(density) for id, density in id_to_density.items()
     }
-    print(short_id_to_density_cm3)
 
     bunch_fn_to_density = {
         f"final_bunch_{id}.txt": density
         for id, density in short_id_to_density_cm3.items()
     }
-    print()
-    print(bunch_fn_to_density)
 
-    # 'final_bunch_e0cfcf.txt': 4e+17
+    runs_dir = pathlib.Path.home() / "tmp" / "runs"
+    txt_files = runs_dir.glob("final_bunch_*.txt")
+
+    for fn in txt_files:
+        print(f"{fn.name}: {bunch_fn_to_density[fn.name]:.1e}")
 
 
 if __name__ == "__main__":
