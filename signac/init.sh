4e929bbd56ff38c54859e8738bdc421979df9988
Andrei Berceanu <andreicberceanu@gmail.com>
2021-01-27 14:49:46 +0200

    Remove statepoints and slurm logs

--- signac/init.sh
+++ signac/init.sh
@@ -4,9 +4,11 @@
 # Usage: ./init.sh
 
 rm -rf .bundles/
+rm -f slurm-*.out
 rm -f fbpic-project.log
 rm -f signac.rc
 rm -f .signac_sp_cache.json.gz
+rm -f signac_statepoints.json
 rm -f signac_project_document.json
 rm -rf /scratch/berceanu/runs/signac-driven-fbpic/workspace_pwfa/
 
