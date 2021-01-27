5e0527ec3f50f30c08ee138f3d53de07b8512c33
Andrei Berceanu <andreicberceanu@gmail.com>
2021-01-27 23:04:54 +0200

    Save densities and centroid averages to txt file

--- signac/src/centroid.py
+++ signac/src/centroid.py
@@ -228,6 +228,7 @@ def main():
         pyplot.close(fig)
 
     x, y = job_densities, job_centroid_positions
+    np.savetxt('average_centroids.txt', np.c_[x,y])
 
     fig, ax = pyplot.subplots(figsize=(golden * 4, 4))
     ax.plot(x, y, "C1o:", mec="1.0",)
