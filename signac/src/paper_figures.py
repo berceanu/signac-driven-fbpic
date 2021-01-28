1c6bb8384e0aedef46f53d848d228728b463fb61
Andrei Berceanu <andreicberceanu@gmail.com>
2021-01-28 16:31:38 +0200

    Refactor into function

--- signac/src/paper_figures.py
+++ signac/src/paper_figures.py
@@ -22,10 +22,10 @@ def ticks_format(value, index):
         return "${0:d}\\times10^{{{1:d}}}$".format(int(base), int(exp))
 
 
-def main():
-    x, y = np.loadtxt("average_centroids.txt", unpack=True)
+def plot_average_centroids(x, y, ax, x_scale_is_log=True, save_as_png=True):
+    ax.set_xlabel(r"$n_e$ ($\mathrm{cm^{-3}}$)")
+    ax.set_ylabel(r"$\langle x \rangle$ ($\mathrm{\mu m}$)")
 
-    fig, ax = pplt.subplots(aspect=(golden * 3, 3))
     ax.plot(
         x,
         y,
@@ -33,24 +33,42 @@ def main():
         mec="1.0",
     )
 
-    ax.set_xscale("log")
-    ax.set_xlabel(r"$n_e$ ($\mathrm{cm^{-3}}$)")
-    ax.set_ylabel(r"$\langle x \rangle$ ($\mathrm{\mu m}$)")
-
     ax.grid(which="both")
-
-    subs = [2.0, 4.0, 6.0]  # ticks to show per decade
-    ax.xaxis.set_minor_locator(ticker.LogLocator(subs=subs))  # set the ticks position
-    ax.xaxis.set_major_formatter(
-        ticker.LogFormatterMathtext()
-    )  # remove the major ticks
-    ax.xaxis.set_minor_formatter(
-        ticker.FuncFormatter(ticks_format)
-    )  # add the custom ticks
     ax.tick_params(axis="x", which="minor", labelsize=5)
 
-    # TODO: remove extension
-    fig.savefig("average_centroids_cut_paper.png", transparent=False)
+    if x_scale_is_log:
+        log_or_lin = "log"
+        ax.set_xscale("log")
+
+        subs = [2.0, 4.0, 6.0]  # ticks to show per decade
+        ax.xaxis.set_minor_locator(
+            ticker.LogLocator(subs=subs)
+        )  # set the ticks position
+        ax.xaxis.set_minor_formatter(
+            ticker.FuncFormatter(ticks_format)
+        )  # add the custom ticks
+        ax.xaxis.set_major_formatter(ticker.LogFormatterMathtext())
+    else:  # linear scale on x axis
+        log_or_lin = "linear"
+        formatter = ticker.ScalarFormatter()
+        formatter.set_useMathText(True)
+        formatter.set_useOffset(False)
+        ax.xaxis.set_minor_formatter(formatter)
+        ax.xaxis.set_major_formatter(formatter)
+
+    if save_as_png:
+        ax.figure.savefig(
+            f"average_centroids_cut_{log_or_lin}_paper.png", transparent=False
+        )
+    else:  # save as PDF
+        ax.figure.savefig(f"average_centroids_cut_{log_or_lin}_paper")
+
+
+def main():
+    x, y = np.loadtxt("average_centroids.txt", unpack=True)
+
+    fig, ax = pplt.subplots(aspect=(golden * 3, 3))
+    plot_average_centroids(x, y, ax, x_scale_is_log=True, save_as_png=False)
 
 
 if __name__ == "__main__":
