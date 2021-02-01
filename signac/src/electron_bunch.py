a12125d9a97e4c2fa4f0c2e92167389fbaca6f2e
Andrei Berceanu <andreicberceanu@gmail.com>
2021-02-02 00:32:01 +0200

    Remove some TODOs

--- signac/src/electron_bunch.py
+++ signac/src/electron_bunch.py
@@ -358,8 +358,6 @@ def main():
     # https://github.com/fbpic/fbpic/issues/385
     # TODO: use Gaussian density profile with 2 ramps, close to experiment
     # TODO: use Gaussian bunch, with parameters close to the experimental one
-    # TODO: can we see hosing? can we see self-modulation?
-    # TODO: find computable quantity in Remi's paper
 
 
 if __name__ == "__main__":
