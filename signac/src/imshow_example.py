3ff8056867e837a051bf15c47bfe1d1fcee4ae50
Andrei Berceanu <andreicberceanu@gmail.com>
2021-02-05 00:43:44 +0200

    Use 16x16 grid

--- signac/src/imshow_example.py
+++ signac/src/imshow_example.py
@@ -3,22 +3,19 @@ from matplotlib import pyplot
 
 
 def main():
-    x, y = np.ogrid[0:6, 0:7]
+    left = -0.5
+    bottom = -0.5
+    right = 15.5
+    top = 15.5
+
+    y, x = np.ogrid[
+        int(bottom + 0.5) : int(top + 0.5), int(left + 0.5) : int(right + 0.5)
+    ]
     data = x + y
 
-    print(data)
-    print(data.shape)
-    # [[ 0  1  2  3  4  5  6]
-    # [ 1  2  3  4  5  6  7]
-    # [ 2  3  4  5  6  7  8]
-    # [ 3  4  5  6  7  8  9]
-    # [ 4  5  6  7  8  9 10]
-    # [ 5  6  7  8  9 10 11]]
-    # (6, 7)
 
     fig, ax = pyplot.subplots()
-    ax.imshow(data, origin="lower", extent=(-0.5, 6.5, -0.5, 5.5))
-    # extent=(left, right, bottom, top)
+    ax.imshow(data, origin="lower", extent=(left, right, bottom, top))
 
     fig.savefig("imshow.png")
 
