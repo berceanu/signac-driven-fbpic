5a70dbbe35e686d119075a2e9a576c783612b98b
Andrei Berceanu <andreicberceanu@gmail.com>
2021-02-05 14:34:40 +0200

    make CUDA and gpustat use same GPU index

--- signac/project.sh
+++ signac/project.sh
@@ -3,5 +3,8 @@
 # Run the project operations, in parallel, using at most N GPUs.
 # Usage: ./project.sh N
 
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+
 # parallel on N GPUs
 python src/project.py submit -o ex --bundle=$1 --parallel
+
