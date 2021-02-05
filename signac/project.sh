6d021ef31481d681392fccfac052266589104793
Andrei Berceanu <andreicberceanu@gmail.com>
2021-02-06 00:05:36 +0200

    Reduce sleep time

--- signac/project.sh
+++ signac/project.sh
@@ -8,4 +8,4 @@ export CUDA_DEVICE_ORDER=PCI_BUS_ID
 # parallel on N GPUs
 python src/project.py submit -o ex --bundle=$1 --parallel
 
-( sleep 120 ; python src/nvml.py > /dev/null 2>&1 & ) &
+( sleep 65 ; python src/nvml.py > /dev/null 2>&1 & ) &
