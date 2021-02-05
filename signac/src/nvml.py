59681737fd46db8d1c6e4c0906cbbfb7f16d0bb3
Andrei Berceanu <andreicberceanu@gmail.com>
2021-02-05 19:41:16 +0200

    v1.0

--- signac/src/nvml.py
+++ signac/src/nvml.py
@@ -1,29 +1,58 @@
 import pynvml
 import pathlib
+import datetime
+import time
+
+
+def is_python_process(pid):
+    """Checks process identified by PID is a python process."""
+    name = pynvml.nvmlSystemGetProcessName(pid)
+    p = pathlib.Path(name.decode("UTF-8"))
+    return p.stem == "python"
+
+
+def is_python_running(number_of_gpus):
+    """Return True if there's *at least 1* running python process on *any* of the GPUs."""
+    for gpu in range(number_of_gpus):
+        handle = pynvml.nvmlDeviceGetHandleByIndex(gpu)
+        processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
+        for p in processes:
+            if is_python_process(p.pid):
+                return True
+    return False
+
 
 def main():
     pynvml.nvmlInit()
-    deviceCount = pynvml.nvmlDeviceGetCount()
+    gpu_count = pynvml.nvmlDeviceGetCount()
 
-    version = pynvml.nvmlSystemGetDriverVersion().decode('UTF-8')
-    print(f"Driver Version: {version}")
+    out_file = pathlib.Path.cwd() / "nvml.csv"
 
-    for i in range(deviceCount):
-        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+    with out_file.open("w") as f:
+        f.write(
+            "time_stamp, gpu_uuid, pid, used_gpu_memory_MiB, used_power_W, GPU_Util_%\n"
+        )
 
-        info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-        uuid = pynvml.nvmlDeviceGetUUID(handle).decode('UTF-8')
+    while is_python_running(gpu_count):
+        time.sleep(10)
+        for gpu in range(gpu_count):
+            handle = pynvml.nvmlDeviceGetHandleByIndex(gpu)
+            processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
+            for p in processes:
+                if is_python_process(p.pid):
+                    mem_MiB = p.usedGpuMemory / 1024 / 1024
 
-        procs = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
-        if procs:
-            print(f"{uuid}: {info.used / 1024 / 1024}")
-            for p in procs:
-                name = pynvml.nvmlSystemGetProcessName(p.pid)
-                p_path = pathlib.Path(name.decode('UTF-8'))
-                mem_MiB = p.usedGpuMemory / 1024 / 1024
-                print(p_path.stem, mem_MiB)
-                if p_path.stem == "python":
-                    print(f"{uuid}: {mem_MiB} MiB")
+                    time_stamp = datetime.datetime.now()
+                    uuid = pynvml.nvmlDeviceGetUUID(handle).decode("UTF-8")
+                    pow_draw_watt = pynvml.nvmlDeviceGetPowerUsage(handle) / 1000.0
+                    gpu_util_percentage = pynvml.nvmlDeviceGetUtilizationRates(
+                        handle
+                    ).gpu
+
+                    with out_file.open("a") as f:
+                        f.write(
+                            f"{time_stamp}, {uuid}, {p.pid}, {mem_MiB:g}, {pow_draw_watt}, {gpu_util_percentage}\n"
+                        )
 
     pynvml.nvmlShutdown()
 
