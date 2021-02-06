eead42bbb5591142f807e31891606f21e5779f00
Andrei Berceanu <andreicberceanu@gmail.com>
2021-02-06 14:41:23 +0200

    Also delete signac shell history

--- signac/init.sh
+++ signac/init.sh
@@ -8,6 +8,7 @@ rm -f fbpic-project.log
 rm -f slurm*
 rm -f signac.rc
 rm -f .signac_sp_cache.json.gz
+rm -f .signac_shell_history
 rm -f signac_project_document.json
 rm -f signac_statepoints.json
 rm -rf /scratch/berceanu/runs/signac-driven-fbpic/workspace_lwfa/
