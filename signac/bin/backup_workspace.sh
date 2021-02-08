237c262579b1c79338a2b512306da961662d3f78
Andrei Berceanu <andreicberceanu@gmail.com>
2021-02-08 20:31:07 +0200

    Add bash script for archiving workspace

---
+++ signac/bin/backup_workspace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PR_DIR=$(sed -n '2p' signac.rc | cut -d "=" -f2 | xargs)
+SOURCE=${PR_DIR%/}
+echo "${SOURCE}"
+
+BACKUP_DIR="$(dirname "${SOURCE}")"
+BACKUP_FILE="$(basename "${SOURCE}")"
+DATE=$(date +%Y-%m-%d-%H%M%S)
+echo "${BACKUP_DIR}/${BACKUP_FILE}-${DATE}.tar.gz"
+
+tar -cvzpf ${BACKUP_DIR}/${BACKUP_FILE}-${DATE}.tar.gz ${SOURCE}
