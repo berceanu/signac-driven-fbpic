1707caf0386676fb436a03112dae6b1355b92fb9
Andrei Berceanu <andreicberceanu@gmail.com>
2021-02-10 22:58:02 +0200

    Add throttling info

--- signac/bin/nvml.py
+++ signac/bin/nvml.py
@@ -8,32 +8,39 @@ import pathlib
 import datetime
 import schedule
 
-# Accomodate more than one `fbpic` run per GPU
-# Accomodate PIConGPU usecase => track all 16 GPUs; don't search for Python processes only
-
-# TODO comput ms/step and total particle count and save in job document
 
 def job(gpu_count, out_file):
     for gpu in range(gpu_count):
         handle = pynvml.nvmlDeviceGetHandleByIndex(gpu)
         processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
+        supported_throttle_reasons = pynvml.nvmlDeviceGetSupportedClocksThrottleReasons(
+            handle
+        )
+        throttle_reasons = pynvml.nvmlDeviceGetCurrentClocksThrottleReasons(handle)
         for p in processes:
             mem_MiB = p.usedGpuMemory / 1024 / 1024
 
             time_stamp = datetime.datetime.now()
             uuid = pynvml.nvmlDeviceGetUUID(handle).decode("UTF-8")
             pow_draw_watt = pynvml.nvmlDeviceGetPowerUsage(handle) / 1000.0
-            gpu_util_percentage = pynvml.nvmlDeviceGetUtilizationRates(
-                handle
-            ).gpu
+            gpu_util_percentage = pynvml.nvmlDeviceGetUtilizationRates(handle).gpu
+
+            throtttle_reasons = {
+                "sw_power_cap": pynvml.nvmlClocksThrottleReasonSwPowerCap,
+                "hw_slowdown": pynvml.nvmlClocksThrottleReasonHwSlowdown,
+            }
+            throtttle_state = dict()
+            for name, mask in throtttle_reasons.items():
+                if mask & supported_throttle_reasons:
+                    if mask & throttle_reasons:
+                        throtttle_state[name] = "A"  # Active
+                    else:
+                        throtttle_state[name] = "NA"  # Not Active
 
             with out_file.open("a") as f:
                 f.write(
-                    f"{time_stamp},{uuid},{p.pid},{mem_MiB:g},{pow_draw_watt},{gpu_util_percentage}\n"
+                    f"{time_stamp},{uuid},{p.pid},{mem_MiB:g},{pow_draw_watt},{gpu_util_percentage},{throtttle_state['sw_power_cap']},{throtttle_state['hw_slowdown']}\n"
                 )
-
-
-# TODO add other metrics like temperature, throttling state and/of clock freq
 
 
 def main():
@@ -46,7 +53,9 @@ def main():
 
     # write CSV header
     with out_file.open("w") as f:
-        f.write("time_stamp,gpu_uuid,pid,used_gpu_memory_MiB,used_power_W,GPU_Util_%\n")
+        f.write(
+            "time_stamp,gpu_uuid,pid,used_gpu_memory_MiB,used_power_W,GPU_Util_%,sw_power_cap,hw_slowdown\n"
+        )
 
     schedule.every(10).seconds.do(job, gpu_count=gpu_count, out_file=out_file)
 
