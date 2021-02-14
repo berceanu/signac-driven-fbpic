830b9c7f398b88eb003ed263f545bdd4812546a4
Andrei Berceanu <andreicberceanu@gmail.com>
2021-02-14 23:10:50 +0200

    Minor refactoring

--- signac/src/electron_spectrum.py
+++ signac/src/electron_spectrum.py
@@ -47,6 +47,8 @@ class ElectronSpectrum:
     xlabel: str = "$E$ (MeV)"
     xlim: EnergyWindow = EnergyWindow(50.0, 350.0)
     hatch_window: EnergyWindow = EnergyWindow(100.0, 300.0)
+    peak_position: float = field(init=False)
+    sigma: float = 10.0  # std of Gaussian Kernel
     ylabel: str = "$\\frac{\\mathrm{d} Q}{\\mathrm{d} E}$ (pC/MeV)"
     ylim: ChargeWindow = ChargeWindow(0.0, 50.0)
     linewidth: float = 0.5
@@ -55,6 +57,10 @@ class ElectronSpectrum:
 
     def __post_init__(self):
         self.differential_charge, self.energy = self.loadf()
+        self.smooth_differential_charge = gaussian_filter1d(
+            self.differential_charge, self.sigma
+        )
+        self.peak_position = self.find_peak_in_window()
 
     def loadf(self):
         f = np.load(self.fname)
@@ -71,8 +77,8 @@ class ElectronSpectrum:
 
     def add_histogram(self):
         self.ax.hist(
-            self.energy,
-            self.energy,
+            x=self.energy,
+            bins=self.energy,
             weights=self.differential_charge,
             histtype="step",
             color=self.linecolor,
@@ -80,9 +86,14 @@ class ElectronSpectrum:
             label="original electron spectrum",
         )
 
-    def add_gaussian_filter(self, sigma=3.0):
-        y = gaussian_filter1d(self.differential_charge, sigma)
-        label = f"filtered, $\\sigma={sigma}$"
+    def add_gaussian_filter(self, sigma=None):
+        if sigma is None:
+            y = self.smooth_differential_charge
+            sigma = self.sigma
+        else:
+            y = gaussian_filter1d(self.differential_charge, sigma)
+
+        label = f"filtered, $\\sigma={sigma:.0f}$"
         self.ax.plot(
             self.energy,
             y,
@@ -91,7 +102,6 @@ class ElectronSpectrum:
             linestyle=STYLE[label]["linestyle"],
             linewidth=2 * self.linewidth,
         )
-        return y
 
     def add_ticks(self, major_x_every=25.0, major_y_every=10.0):
 
@@ -126,8 +136,8 @@ class ElectronSpectrum:
 
     def add_hatch(self):
         self.ax.fill_between(
-            self.energy,
-            self.differential_charge,
+            x=self.energy,
+            y1=self.differential_charge,
             where=(self.energy >= self.hatch_window.low)
             & (self.energy <= self.hatch_window.high),
             facecolor="none",
@@ -137,20 +147,42 @@ class ElectronSpectrum:
             alpha=self.alpha,
         )
 
+    def annotate_peak(self):
+        self.peak_position
+        self.ax
+        self.ax.vlines(
+            x=self.peak_position,
+            ymin=self.ylim.low,
+            ymax=self.ylim.high,
+            colors=self.linecolor,
+            linestyles="dashed",
+            linewidth=self.linewidth,
+            label=f"{self.peak_position:.0f} MeV",
+        )
+
     def plot(self):
         self.prepare_figure()
         self.add_histogram()
-        for sigma in 3, 6, 10:
-            self.add_gaussian_filter(sigma=sigma)
-        self.smooth_differential_charge = self.add_gaussian_filter(sigma=10)
+        # for sigma in 3, 6, 10:
+        # self.add_gaussian_filter(sigma=sigma)
+        self.add_gaussian_filter()
         self.add_ticks()
         self.add_grid()
         self.add_hatch()
+        self.annotate_peak()
         self.ax.legend()
 
-    def save(self, fname="electron_spectrum.png", dpi=192):
+    def savefig(self, fname="electron_spectrum.png", dpi=192):
         self.fig.savefig(fname, dpi=dpi)
         pyplot.close(self.fig)
+
+    def find_peak_in_window(self):
+        mask = (self.energy >= self.hatch_window.low) & (
+            self.energy <= self.hatch_window.high
+        )
+        energy = self.energy[mask]
+        charge = self.smooth_differential_charge[mask]
+        return energy[np.argmax(charge)]
 
 
 def main():
@@ -165,9 +197,10 @@ def main():
     print(job)
 
     es = ElectronSpectrum(job.fn("final_histogram.npz"))
-
     es.plot()
-    es.save()
+    es.savefig()
+
+    # print(es)  # FIXME
 
 
 if __name__ == "__main__":
