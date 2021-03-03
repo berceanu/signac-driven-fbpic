f9148262606e4bd8a8f51ad1fea660bfd87dcc80
Andrei Berceanu <andreicberceanu@gmail.com>
2021-03-03 02:16:32 +0200

    Add imports, docstring

--- signac/src/parameter_scan.py
+++ signac/src/parameter_scan.py
@@ -1,3 +1,14 @@
+"""
+Computes the electron energy spectra for the whole parameter space and loads it
+into the N-dimensional array class.
+"""
+import job_util
+from itertools import product
+from energy_histograms import job_energy_histogram
+from xarray_spectra import XSpectra
+import signac
+
+
 def two_parameters_study(project, keys=("a0", "n_e")):
     spectra = list()
 
@@ -6,13 +17,17 @@ def two_parameters_study(project, keys=("a0", "n_e")):
 
     for val_y, val_x in product(vy, vx):
         job = next(iter(project.find_jobs(filter={keys[0]: val_y, keys[1]: val_x})))
-        spectrum = construct_electron_spectrum(job)
+        spectrum = job_energy_histogram(job)
         spectra.append(spectrum)
 
 
 def main():
-    pass
+    """Main entry point."""
+    proj = signac.get_project(search=False)
+
+    for job in proj:  # ~14s
+        hist = job_energy_histogram(job)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
