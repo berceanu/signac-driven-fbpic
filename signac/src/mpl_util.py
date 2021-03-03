a9fdeba2d64f6316a7ba444bcdc506fc4826a761
Andrei Berceanu <andreicberceanu@gmail.com>
2021-03-03 17:59:16 +0200

    Remove deprecated functions from mpl_util

--- signac/src/mpl_util.py
+++ signac/src/mpl_util.py
@@ -112,35 +112,6 @@ def add_colorbar(ax, mappable, *, size="3%", position="right"):
         )
 
 
-def add_grid(ax, linewidth=0.5, linecolor="0.5"):
-    lw = dict(major=linewidth, minor=linewidth / 2)
-    for xy in "x", "y":
-        for ticks in "major", "minor":
-            ax.grid(
-                which=ticks,
-                axis=xy,
-                linewidth=lw[ticks],
-                linestyle="dotted",
-                color=linecolor,
-            )
-
-
-def add_ticks(ax, major_x_every=25.0, major_y_every=10.0, alpha=0.75):
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(major_x_every))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(major_y_every))
-    ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
-    ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
-
-    for ticks, length, width in zip(("major", "minor"), (6, 3), (2, 1)):
-        ax.tick_params(
-            which=ticks,
-            direction="in",
-            length=length,
-            width=width,
-            grid_alpha=alpha,
-        )
-
-
 def main():
     """Main entry point."""
 
