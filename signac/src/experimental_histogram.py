1809e32fd8ea509af12bf37c7dfc5f8edfb4f2b9
Andrei Berceanu <andreicberceanu@gmail.com>
2021-03-16 18:54:02 +0200

    Reformating

--- signac/src/experimental_histogram.py
+++ signac/src/experimental_histogram.py
@@ -1,41 +1,42 @@
-import pandas as pd
+"""
+Postprocessing and visualizing experimental data, such as the electron spectrum/histogram.
+"""
 import pathlib
-from matplotlib import cm, colors, figure, rc_context, ticker
-from matplotlib.backends.backend_agg import FigureCanvasAgg
+
 import numpy as np
-from fast_histogram import histogram1d
-import math
+import pandas as pd
+from matplotlib import figure, rc_context
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
 import mpl_util
 import util
 
+
 def read_spectrum(path_to_csv):
-    df = pd.read_csv(
+    """Read spectrum data from CSV file, return dataframe."""
+    csv_df = pd.read_csv(
         path_to_csv,
         comment="#",
         names=["E_MeV_float", "dN_over_dE"],
     )
-    df.loc[:, "E_MeV"] = df.loc[:, "E_MeV_float"].astype(int)
-    grouped = df.groupby(["E_MeV"])
+    csv_df.loc[:, "E_MeV"] = csv_df.loc[:, "E_MeV_float"].astype(int)
+    grouped = csv_df.groupby(["E_MeV"])
     df_ = grouped[["dN_over_dE"]].agg(["mean"])
     df_.columns = df_.columns.get_level_values(0)
     df_["dN_over_dE"] = df_["dN_over_dE"].astype(np.float64)
-    df_["scaled_dN_over_dE"] = util.normalize_to_interval(0,1,df_["dN_over_dE"])
+    df_["dN_over_dE_normalized"] = util.normalize_to_interval(0, 1, df_["dN_over_dE"])
 
     return df_
 
 
-
 def plot_spectrum(df, ax):
-    ax.step(
-        df.index,
-        df.scaled_dN_over_dE
-    )
+    ax.step(df.index, df.dN_over_dE_normalized)
     ax.set_xlabel("E (MeV)")
     ax.set_ylabel("dN/dE (a.u.)")
 
+
 def main():
     """Main entry point."""
-
 
     csv_path = pathlib.Path.cwd() / "experimental_spectrum.csv"
     df = read_spectrum(csv_path)
@@ -44,11 +45,11 @@ def main():
         mpl_util.mpl_publication_style()
 
         fig = figure.Figure()
-        canvas = FigureCanvasAgg(fig)
+        _ = FigureCanvasAgg(fig)
         ax = fig.add_subplot(111)
 
         plot_spectrum(df, ax)
-        fig.savefig("exp")
+        fig.savefig("spectrum_exp")
 
 
 if __name__ == "__main__":
