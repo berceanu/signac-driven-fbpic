fc5456ebc99f81a1fa671f57e02a37f14fe4fd4f
Andrei Berceanu <andreicberceanu@gmail.com>
2021-03-16 18:31:27 +0200

    Plot experimental histogram

--- signac/src/experimental_histogram.py
+++ signac/src/experimental_histogram.py
@@ -3,32 +3,53 @@ import pathlib
 from matplotlib import cm, colors, figure, rc_context, ticker
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 import numpy as np
+from fast_histogram import histogram1d
+import math
+import mpl_util
+import util
 
 def read_spectrum(path_to_csv):
     df = pd.read_csv(
-    path_to_csv,
-    comment='#',
-    names=["E_MeV", "dN_over_dE"],
+        path_to_csv,
+        comment="#",
+        names=["E_MeV_float", "dN_over_dE"],
     )
-    return df
+    df.loc[:, "E_MeV"] = df.loc[:, "E_MeV_float"].astype(int)
+    grouped = df.groupby(["E_MeV"])
+    df_ = grouped[["dN_over_dE"]].agg(["mean"])
+    df_.columns = df_.columns.get_level_values(0)
+    df_["dN_over_dE"] = df_["dN_over_dE"].astype(np.float64)
+    df_["scaled_dN_over_dE"] = util.normalize_to_interval(0,1,df_["dN_over_dE"])
+
+    return df_
+
+
 
 def plot_spectrum(df, ax):
-    ax.plot("E_MeV", "dN_over_dE", data=df)
-    
+    ax.step(
+        df.index,
+        df.scaled_dN_over_dE
+    )
+    ax.set_xlabel("E (MeV)")
+    ax.set_ylabel("dN/dE (a.u.)")
 
 def main():
     """Main entry point."""
 
-    fig = figure.Figure(figsize=(20, 8), dpi=192)
-    canvas = FigureCanvasAgg(fig)
-    ax = fig.add_subplot(111)
 
     csv_path = pathlib.Path.cwd() / "experimental_spectrum.csv"
     df = read_spectrum(csv_path)
 
-    plot_spectrum(df, ax)
+    with rc_context():
+        mpl_util.mpl_publication_style()
 
-    fig.savefig("exp.png")
+        fig = figure.Figure()
+        canvas = FigureCanvasAgg(fig)
+        ax = fig.add_subplot(111)
 
-if __name__ == '__main__':
+        plot_spectrum(df, ax)
+        fig.savefig("exp")
+
+
+if __name__ == "__main__":
     main()
