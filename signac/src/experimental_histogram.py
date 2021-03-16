a4cb7f039c86159186bc6bf67fc1ae828b84e701
Andrei Berceanu <andreicberceanu@gmail.com>
2021-03-16 20:14:17 +0200

    Add spectrum colormap

--- signac/src/experimental_histogram.py
+++ signac/src/experimental_histogram.py
@@ -5,7 +5,7 @@ import pathlib
 
 import numpy as np
 import pandas as pd
-from matplotlib import figure, rc_context
+from matplotlib import cm, colors, figure, rc_context, ticker
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 import mpl_util
@@ -30,10 +30,51 @@ def read_spectrum(path_to_csv):
 
 
 def plot_spectrum(spectrum, axes):
-    """Visualize spectrum."""
-    axes.step(spectrum.index, spectrum.dN_over_dE_normalized, "black")
+    """Visualize spectrum as a line."""
+    axes.step(spectrum.index.values, spectrum.dN_over_dE_normalized.values, "black")
     axes.set_xlabel(r"$E$ ($\mathrm{MeV}$)")
     axes.set_ylabel(r"$\frac{\mathrm{d} N}{\mathrm{d} E}$ ($\mathrm{a.u.}$)")
+    return axes
+
+
+def pcolor_spectrum(spectrum, axes):
+    """Visualize spectrum as color band."""
+    img = axes.pcolorfast(
+        spectrum.index.values,
+        [0.0, 1.0],
+        spectrum.dN_over_dE_normalized.values[np.newaxis, :-1],
+        norm=colors.Normalize(vmin=0.0, vmax=1.0),
+        cmap=cm.get_cmap("turbo"),
+        rasterized=True,
+    )
+    axes.yaxis.set(
+        minor_locator=ticker.NullLocator(),
+        minor_formatter=ticker.NullFormatter(),
+        major_locator=ticker.NullLocator(),
+        major_formatter=ticker.NullFormatter(),
+    )
+    for pos in "right", "left", "top", "bottom":
+        axes.spines[pos].set_visible(False)
+    #
+    axes.set_xlabel(r"$E$ ($\mathrm{MeV}$)")
+    cbar = mpl_util.add_colorbar(axes, img)
+    cbar.ax.set_title(r"$\frac{\mathrm{d} N}{\mathrm{d} E}$ ($\mathrm{a.u.}$)")
+    return img, cbar
+
+
+def spectrum_figure(spectrum, plotter):
+    """Create spectrum figure, either 1D or 2D."""
+    with rc_context():
+        mpl_util.mpl_publication_style()
+
+        fig = figure.Figure()
+        _ = FigureCanvasAgg(fig)
+        axs = fig.add_subplot(111)
+
+        plotter(spectrum, axs)
+        fig.savefig(f"spectrum_exp_{plotter.__name__}")
+
+    return fig
 
 
 def main():
@@ -42,15 +83,8 @@ def main():
     csv_path = pathlib.Path.cwd() / "experimental_spectrum.csv"
     spectrum = read_spectrum(csv_path)
 
-    with rc_context():
-        mpl_util.mpl_publication_style()
-
-        fig = figure.Figure()
-        _ = FigureCanvasAgg(fig)
-        axs = fig.add_subplot(111)
-
-        plot_spectrum(spectrum, axs)
-        fig.savefig("spectrum_exp")
+    spectrum_figure(spectrum, plot_spectrum)
+    spectrum_figure(spectrum, pcolor_spectrum)
 
 
 if __name__ == "__main__":
