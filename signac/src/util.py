94d104194a4c8a30aa743cf540eb5447c02a9cfd
Andrei Berceanu <andreicberceanu@gmail.com>
2021-03-18 22:27:51 +0200

    Add slugify

--- signac/src/util.py
+++ signac/src/util.py
@@ -3,10 +3,34 @@ import datetime
 import logging
 import subprocess
 import time
+import unicodedata
+import re
 
 import numpy as np
 
 logger = logging.getLogger(__name__)
+
+
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = (
+            unicodedata.normalize("NFKD", value)
+            .encode("ascii", "ignore")
+            .decode("ascii")
+        )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
+
 
 def normalize_to_interval(a, b, data):
     """Given the `data` array, normalize its values in the [a, b] interval."""
