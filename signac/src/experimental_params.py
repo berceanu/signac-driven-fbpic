4c79901a69afdacbd83e7836d7a63e289e8d1aaf
Andrei Berceanu <andreicberceanu@gmail.com>
2021-03-26 13:45:43 +0200

    Update experimental params

--- signac/src/experimental_params.py
+++ signac/src/experimental_params.py
@@ -6,14 +6,11 @@ SQRT_FACTOR = math.sqrt(2 * math.log(2))
 
 
 def main():
-    laser = lwfa.Laser.from_a0(
-        a0=2.4 * u.dimensionless,
-        τL=27.8 / SQRT_FACTOR * u.femtosecond,
-        beam=lwfa.GaussianBeam(
-            w0=22.0 / SQRT_FACTOR * u.micrometer, λL=815 * u.nanometer
-        ),
-    )
-    plasma = Plasma(n_pe=8.0e18 * u.cm ** (-3))
+    beam = lwfa.GaussianBeam(w0=120 * u.micrometer, λL=800 * u.nanometer)
+    laser = lwfa.Laser(ɛL=8 * u.milijoule, τL=50 * u.femtosecond, beam=beam)
+
+
+    plasma = Plasma(n_pe=2.65e+18 * u.cm ** (-3))
 
     print(laser)
     print(plasma)
