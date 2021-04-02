71cce0440f8b3a2b20c6b24bf2516f974c862c02
Andrei Berceanu <andreicberceanu@gmail.com>
2021-04-02 13:50:40 +0300

    Plot experimental density

--- signac/src/density_functions.py
+++ signac/src/density_functions.py
@@ -1,23 +1,44 @@
 """Repository of `fbpic` density functions."""
 from itertools import cycle
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot
 from matplotlib.gridspec import GridSpec
 
 
-def read_density(txt_file, every_nth=20, offset=0.0):
+def read_longitudinal_profile(txt_file="longitudinal_density.txt", offset=20):
+    return read_density(txt_file=txt_file, offset=offset)
+
+
+def read_transverse_profile(txt_file="transverse_density.txt", offset=-0.25):
+    return read_density(txt_file=txt_file, offset=offset)
+
+
+def plot_profile(position_m, norm_density, axs, y_offset=0.0):
+
+    axs.plot(position_m * 1e6, norm_density + y_offset)
+
+    # axs.set_xlabel("position_m")
+    # axs.set_ylabel("norm_density")
+
+    # axs.set_xlim(-80, 40_000 + 20)
+    # axs.set_ylim(0.0, 1.2)
+
+    return axs
+
+
+def read_density(txt_file, every_nth=1, offset=0.0):
     df = pd.read_csv(
         txt_file,
         delim_whitespace=True,
-        names=["position_mm", "density_cm_3", "error_density_cm_3"],
+        header=0,
     )
+    # substract offset
+    df.position_mm = df.position_mm + offset
 
     # convert to meters
     df["position_m"] = df.position_mm * 1e-3
-
-    # substract offset
-    df.position_m = df.position_m + offset
 
     # normalize density
     df["norm_density"] = df.density_cm_3 / df.density_cm_3.max()
@@ -29,16 +50,13 @@ def read_density(txt_file, every_nth=20, offset=0.0):
     df = df.iloc[::every_nth, :]
 
     # return data as numpy arrays
-    return df.position_m.to_numpy(), df.norm_density.to_numpy()
+    return df.position_m.values, df.norm_density.values
 
 
 def make_gaussian_dens_func(job):
     def ramp(z, *, center, sigma, p):
         """Gaussian-like function."""
         return np.exp(-((np.abs(z - center) / sigma) ** p))
-
-    w_matched = 50.e-6
-    rel_delta_n_over_w2 = 1./( np.pi * 2.81e-15 * job.sp.w0**4 * job.sp.n_e )
 
     # The density profile
     def dens_func(z, r):
@@ -85,7 +103,8 @@ def make_gaussian_dens_func(job):
         n = np.where(z >= job.sp.center_right + 2 * job.sp.sigma_right, 0, n)
 
         # Add transverse guiding parabolic profile
-        n = n * ( 1. + rel_delta_n_over_w2 * r**2 )
+        n = n * (1.0 + 0.25 * r ** 2 / job.sp.rmax ** 2)
+
         return n
 
     return dens_func
@@ -103,23 +122,30 @@ def plot_density_profile(profile_maker, fig_fname, job):
 
     num = int((job.sp.L_interact - job.sp.zmin) * 1e6 / 100 + 1)
     all_z = np.linspace(job.sp.zmin, job.sp.L_interact, num)
-    dens = profile_maker(job)(all_z, 0.0)
+    all_r = np.linspace(-job.sp.rmax, job.sp.rmax, 512)
+    dens_z = profile_maker(job)(all_z, 0.0)
+    dens_r = profile_maker(job)(20e-3, all_r)
 
-    fig = pyplot.figure(figsize=(30, 4.8))
+    fig = pyplot.figure(figsize=(30, 6.8))
     G = GridSpec(2, 1, figure=fig)
     ax_top = fig.add_subplot(G[0, :])
     ax_bottom = fig.add_subplot(G[1, :])
 
     for ax in (ax_top, ax_bottom):
-        ax.plot(all_z * 1e6, dens, marker="o", linestyle="", markersize=5, alpha=0.2)
-        ax.fill_between(all_z * 1e6, dens, alpha=0.3)
-        ax.set_xlabel(r"$%s \;(\mu m)$" % "z")
-        ax.set_ylim(0.0, 1.2)
-        ax.set_xlim(left=job.sp.zmin * 1e6 - 20)
+        ax.set_ylim(0.0, 1.25)
         ax.set_ylabel("Density profile $n$")
 
-    ax_top.set_xlim(right=job.sp.L_interact * 1e6 + 20)
-    ax_bottom.set_xlim(right=job.sp.center_left * 1e6)
+    ax_top.plot(all_z * 1e6, dens_z, marker="o", linestyle="", markersize=5, alpha=0.2)
+    ax_top.fill_between(all_z * 1e6, dens_z, alpha=0.3)
+    ax_top.set_xlim(job.sp.zmin * 1e6 - 20, job.sp.L_interact * 1e6 + 20)
+    ax_top.set_xlabel(r"$%s \;(\mu m)$" % "z")
+
+    ax_bottom.plot(
+        all_r * 1e6, dens_r, marker="o", linestyle="", markersize=5, alpha=0.2
+    )
+    ax_bottom.fill_between(all_r * 1e6, dens_r, alpha=0.3)
+    ax_bottom.set_xlim(-job.sp.rmax * 1e6, job.sp.rmax * 1e6)
+    ax_bottom.set_xlabel(r"$%s \;(\mu m)$" % "r")
 
     params_to_annotate = (
         "zmin",
@@ -130,12 +156,11 @@ def plot_density_profile(profile_maker, fig_fname, job):
     )
     y_annotation_positions = (0.5, 0.7, 0.9, 1.1)
     pos_cycle = cycle(y_annotation_positions)
-
+    #
     params_and_positions = [(p, next(pos_cycle)) for p in params_to_annotate]
-
+    #
     for p, y_pos in params_and_positions:
-        for ax in (ax_top, ax_bottom):
-            mark_on_plot(ax=ax, parameter=p, y=y_pos)
+        mark_on_plot(ax=ax_top, parameter=p, y=y_pos)
 
     ax_top.hlines(
         y=1.0,
@@ -146,12 +171,19 @@ def plot_density_profile(profile_maker, fig_fname, job):
         color="0.75",
     )
 
+    position_m, norm_density = read_longitudinal_profile()
+    plot_profile(position_m, norm_density, ax_top)
+
+    position_m, norm_density = read_transverse_profile()
+    plot_profile(position_m, norm_density, ax_bottom, y_offset=0.25)
+
     fig.savefig(fig_fname)
     pyplot.close(fig)
 
 
 def main():
     import random
+
     import signac
 
     random.seed(42)
@@ -159,6 +191,16 @@ def main():
     proj = signac.get_project(search=False)
     ids = [job.id for job in proj]
     job = proj.open_job(id=random.choice(ids))
+
+    fig = pyplot.figure(figsize=(30, 4.8))
+    axs = fig.add_subplot(111)
+    plot_profile(*read_longitudinal_profile(), axs)
+    fig.savefig("longitudinal.png")
+
+    fig = pyplot.figure(figsize=(30, 4.8))
+    axs = fig.add_subplot(111)
+    plot_profile(*read_transverse_profile(), axs)
+    fig.savefig("transverse.png")
 
     plot_density_profile(make_gaussian_dens_func, "initial_density_profile.png", job)
 
