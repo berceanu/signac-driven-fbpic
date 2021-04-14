84b9b203cbff3f3d2f966454fd8c3c3f9e625ac6
Andrei Berceanu <andreicberceanu@gmail.com>
2021-04-14 17:03:18 +0300

    Multiple plots by angle and x_corr func

--- signac/src/x_correlation.py
+++ signac/src/x_correlation.py
@@ -7,9 +7,8 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 from icecream import ic
-from matplotlib import figure
 from matplotlib import rc_context
-from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib import pyplot
 
 import util
 import job_util
@@ -19,8 +18,17 @@ import signac
 
 ic.configureOutput(includeContext=True)
 
+FROM_ENERGY = 71  # MeV
+TO_ENERGY = 500  # MeV
 
-def compute_simulated_spectra(project, *, from_energy, to_energy):
+
+def compute_simulated_spectra(
+    project,
+    *,
+    from_energy,
+    to_energy,
+    cone_aperture=None,
+):
     """
     Loop though the project's statepoints and compute the energy spectrum for each.
     Return all spectra inside an XArray.
@@ -43,7 +51,7 @@ def compute_simulated_spectra(project, *, from_energy, to_energy):
             bins=to_energy - from_energy,
             erange=(from_energy, to_energy),
             normalized=True,
-            cone_aperture=0.01,
+            cone_aperture=cone_aperture,
         )
 
     spectra = xr.DataArray(
@@ -56,20 +64,43 @@ def compute_simulated_spectra(project, *, from_energy, to_energy):
     return spectra
 
 
-def max_cross_correlation(
-    simulated_spectra, experimental_spectrum, cutoff_from_maximum=0.8
+def pearson_correlation(simulated_spectra, experimental_spectrum):
+    return xr.corr(simulated_spectra, experimental_spectrum, dim="E")
+
+
+def weighted_correlation(
+    simulated_spectra, experimental_spectrum, *, cutoff_from_maximum=0.8
 ):
     weights = np.tanh((experimental_spectrum / cutoff_from_maximum) ** 2)
 
-    se = (simulated_spectra * experimental_spectrum).sum(dim="E")
-    s = simulated_spectra.sum(dim="E")
-    e = experimental_spectrum.sum(dim="E")
+    se_sum = (simulated_spectra * experimental_spectrum).sum(dim="E")
+    s_sum = simulated_spectra.sum(dim="E")
+    e_sum = experimental_spectrum.sum(dim="E")
 
-    rd = (se / (s * e)) ** 2
-    cross_corr = (rd * weights).sum(dim="E")
+    relative_deviation = (se_sum / (s_sum * e_sum)) ** 2
+    cross_corr = (relative_deviation * weights).sum(dim="E")
+    return cross_corr
 
-    ind = cross_corr.where(cross_corr == cross_corr.max(), drop=True).squeeze()
-    return ind.power.values.item(), ind.n_e.values.item()
+
+def best_match(cross_correlation_function, simulated_spectra, experimental_spectrum):
+    x_corr = cross_correlation_function(simulated_spectra, experimental_spectrum)
+
+    ind = x_corr.where(x_corr == x_corr.max(), drop=True).squeeze()
+    power, n_e = ind.power.values.item(), ind.n_e.values.item()
+
+    return simulated_spectra.sel(power=power, n_e=n_e, method="nearest")
+
+
+def compute_spectra_and_match(
+    experimental_spectrum, *, cone_aperture=0.01, x_corr_func=weighted_correlation
+):
+    simulated_spectra = compute_simulated_spectra(
+        signac.get_project(search=False),
+        from_energy=FROM_ENERGY,
+        to_energy=TO_ENERGY,
+        cone_aperture=cone_aperture,
+    )
+    return best_match(x_corr_func, simulated_spectra, experimental_spectrum)
 
 
 def plot_experimental_spectrum(axes, spectrum):
@@ -84,10 +115,11 @@ def plot_experimental_spectrum(axes, spectrum):
     return axes
 
 
-def plot_simulated_spectrum(axes, spectrum):
+def plot_simulated_spectrum(axes, spectrum, *, angle):
     label = (
-        f"{spectrum.n_e.attrs['plot_label']} = {spectrum.n_e.values / 1.0e+24}, "
-        f"{spectrum.power.attrs['plot_label']} = {spectrum.power.values:.3f}"
+        f"{spectrum.n_e.attrs['plot_label']} = {spectrum.n_e.values.item() / 1e24}, "
+        f"{spectrum.power.attrs['plot_label']} = {spectrum.power.values.item():.3f}, "
+        f"$2\\theta = {angle * 1e3:.1f}$ mrad"
     )
     axes.step(spectrum.E.values, spectrum.values, label=label)
     axes.legend(fontsize=4)
@@ -117,43 +149,38 @@ def read_experimental_spectrum(path_to_csv, *, from_energy, to_energy):
 
 def main():
     """Main entry point."""
-    FROM_ENERGY = 71  # MeV
-    TO_ENERGY = 500  # MeV
-
     csv_path = pathlib.Path.cwd() / "experimental_spectrum.csv"
     experimental_spectrum = read_experimental_spectrum(
         csv_path, from_energy=FROM_ENERGY, to_energy=TO_ENERGY
     )
 
-    proj = signac.get_project(search=False)
-    simulated_spectra = compute_simulated_spectra(
-        proj, from_energy=FROM_ENERGY, to_energy=TO_ENERGY
-    )
+    matched_spectra = {}
+    for x_corr_foo in (weighted_correlation, pearson_correlation):
+        matched_spectra[x_corr_foo.__name__] = {}
+        for aperture in (None, 0.01):
+            selected_simulated_spectrum = compute_spectra_and_match(
+                experimental_spectrum,
+                cone_aperture=aperture,
+                x_corr_func=x_corr_foo,
+            )
+            matched_spectra[x_corr_foo.__name__][aperture] = selected_simulated_spectrum
 
-    # manual selection
-    # optim_power, optim_density = 3.0, 8.1e24
+    for x_corr_foo in matched_spectra:
+        with rc_context():
+            mpl_util.mpl_publication_style()
 
-    # automatic selection
-    optim_power, optim_density = max_cross_correlation(
-        simulated_spectra,
-        experimental_spectrum,
-    )
+            fig, axs = pyplot.subplots()
 
-    selected_simulated_spectrum = simulated_spectra.sel(
-        power=optim_power, n_e=optim_density, method="nearest"
-    )
+            axs = plot_experimental_spectrum(axs, experimental_spectrum)
+            for aperture in matched_spectra[x_corr_foo]:
+                axs, label = plot_simulated_spectrum(
+                    axs,
+                    matched_spectra[x_corr_foo][aperture],
+                    angle=aperture,
+                )
 
-    with rc_context():
-        mpl_util.mpl_publication_style()
-
-        fig = figure.Figure()
-        _ = FigureCanvasAgg(fig)
-        axs = fig.add_subplot(111)
-
-        axs = plot_experimental_spectrum(axs, experimental_spectrum)
-        axs, label = plot_simulated_spectrum(axs, selected_simulated_spectrum)
-
-        axs.figure.savefig(util.slugify(label) + ".png")
+            axs.set_title(f"{x_corr_foo}")
+            fig.savefig(f"{x_corr_foo}.png")
 
 
 if __name__ == "__main__":
